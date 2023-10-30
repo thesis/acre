3ba358dfb5642b412ee10a8a4fa34fa15657209e
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2023-10-30 13:03:39 +0100

    Update `TokenStaking` contract
    Add support for `approveAndCall`/`receiveApproval` pattern. The tBTC
    token contract that will be a staking token supports this pattern. To be
    able to stake in one transaction (instead of 2: approve + stake) we must
    implement the `RecieveApproval` interface. The token staking contract
    receives approval to spend tokens and create a stake for a given
    account.

---
+++ core/contracts/shared/IReceiveApproval.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.20;
+
+/// @notice An interface that should be implemented by contracts supporting
+///         `approveAndCall`/`receiveApproval` pattern.
+interface IReceiveApproval {
+    /// @notice Receives approval to spend tokens. Called as a result of
+    ///         `approveAndCall` call on the token.
+    function receiveApproval(
+        address from,
+        uint256 amount,
+        address token,
+        bytes calldata extraData
+    ) external;
+}
--- core/contracts/test/TestToken.sol
+++ core/contracts/test/TestToken.sol
@@ -3,11 +3,30 @@
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../shared/IReceiveApproval.sol";
+
 
 contract Token is ERC20 {
     constructor() ERC20("Test Token", "TEST") {}
 
     function mint(address account, uint256 value) external {
         _mint(account, value);
+    }
+
+    function approveAndCall(
+        address spender,
+        uint256 amount,
+        bytes memory extraData
+    ) external returns (bool) {
+        if (approve(spender, amount)) {
+            IReceiveApproval(spender).receiveApproval(
+                msg.sender,
+                amount,
+                address(this),
+                extraData
+            );
+            return true;
+        }
+        return false;
     }
 }
