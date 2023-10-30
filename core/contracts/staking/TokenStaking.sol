8e9e5135a1db3328609a24eb10618470bf9e1d05
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2023-10-30 13:03:32 +0100

    Add `@openzeppelin/contracts` dependency
    Use ERC20 contracts and libraries to interact with staking token in
    staking contract.

--- core/contracts/staking/TokenStaking.sol
+++ core/contracts/staking/TokenStaking.sol
@@ -1,16 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /// @title TokenStaking
 /// @notice A token staking contract for a specified standard ERC20 token. A
 ///         holder of the specified token can stake its tokens to this contract
 ///         and recover the stake after undelegation period is over.
 contract TokenStaking {
-    // TODO: use IERC20 contract as type
-    address internal immutable token;
+    using SafeERC20 for IERC20;
+    
+    IERC20 internal immutable token;
 
-    constructor(address _token) {
+    constructor(IERC20 _token) {
         require(
             address(_token) != address(0),
             "Token can not be the zero address"
