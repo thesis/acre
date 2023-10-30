570af0acac68f69b6e92f0d4c04d2d670af371eb
Dmitry <dymitr.paremski@thesis.co>
2023-10-30 17:21:47 +0100

    Setting up HH framework for Litmus core development
    - Added new hh toolbox
    - Added deployemnt plugin
    - Added helper scripts
    - Added linters
    - Added placeholder dirs for contracts and tests

---
+++ core/scripts/deploy.ts
@@ -0,0 +1,13 @@
+import type { HardhatRuntimeEnvironment } from "hardhat/types"
+import type { DeployFunction } from "hardhat-deploy/types"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { helpers, deployments } = hre
+  const { log } = deployments
+
+  log("Deploying Litmus contract")
+}
+
+export default func
+
+func.tags = ["Litmus"]
