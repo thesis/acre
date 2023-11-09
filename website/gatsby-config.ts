fc01674855a16475a88daa10acf2a13d86d86d41
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-11-09 10:35:23 +0100

    Update a `gatsby-config.ts` for acre website
    - Add a comment about updating needed data
    - Remove an unnecessary comment

--- website/gatsby-config.ts
+++ website/gatsby-config.ts
@@ -2,11 +2,9 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
+    // TODO: Update the needed data when the final domain name will be ready.
     siteUrl: `https://www.yourdomain.tld`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [],
 }
