4f3c72d5799372b4c1cca486a949214aebb7baaf
Dmitry <dymitr.paremski@thesis.co>
2023-11-09 13:45:26 +0100

    Adding foundry for acre
    Acre project can take advantage of foundry framework to write tests in
    Solidity. Acre now will use a hybrid approach by having tests written in
    Solidity and deployment scripts written in TS using HH deployment
    plugin.

--- core/hardhat.config.ts
+++ core/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config"
 
 import "@nomicfoundation/hardhat-toolbox"
+import "@nomicfoundation/hardhat-foundry"
 import "hardhat-deploy"
 
 const config: HardhatUserConfig = {
