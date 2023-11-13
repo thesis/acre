b0699495489aee49bb4823cc12ac56309811560b
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-11-13 15:35:58 +0100

    Add a basic configuration for ledger live app

--- dapp/src/main.tsx
+++ dapp/src/main.tsx
@@ -1,9 +1,24 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import { LedgerWalletAPIProvider } from "./providers"
+
+function DAppProvider() {
+  const params = new URLSearchParams(window.location.search)
+  const isEmbed = params.get("embed")
+
+  if (isEmbed) {
+    return (
+      <LedgerWalletAPIProvider>
+        <App />
+      </LedgerWalletAPIProvider>
+    )
+  }
+  return <App />
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <DAppProvider />
   </React.StrictMode>,
 )
