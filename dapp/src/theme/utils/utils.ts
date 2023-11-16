14a1bb0949d36f362409e67e5fa12acb42810d55
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-11-16 20:23:45 +0100

    Hide a change of theme mode under the feature flag
    Currently, the designs for the dark mode are not yet fully ready. Let's hide it under the feature flag to make sure that the page is displayed correctly.

--- dapp/src/theme/utils/utils.ts
+++ dapp/src/theme/utils/utils.ts
@@ -1,7 +1,7 @@
 // For the ledger live app, we should detect the theme set for the user.
 export function getThemeConfig(theme: string | null) {
     return {
-      initialColorMode: theme,
+      initialColorMode: import.meta.env.VITE_SUPPORT_THEME_MODE === "true" ? theme : "light",
       useSystemColorMode: false,
     }
   }
