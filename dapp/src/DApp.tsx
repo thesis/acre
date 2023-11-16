e2cf153f5da1ef65c71d8f882a11aac6ccfaf99a
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-11-16 13:48:18 +0100

    Remove `embed` flag for dApp
    We had to choose non-dApp section because dApp section didn't support sending bitcoin transactions. In fact,We wanted to use this flag in case we wanted to create a separate app without a ledger live in the future. Let's remove it and come back to it when needed.

--- dapp/src/DApp.tsx
+++ dapp/src/DApp.tsx
@@ -1,24 +1,16 @@
 import React from "react"
-import { useEmbedFeatureFlag } from "./hooks"
 import { LedgerWalletAPIProvider } from "./providers"
 
 function DApp() {
-  const { isEmbed } = useEmbedFeatureFlag()
-  if (isEmbed === "true") return <h1>Ledger live - Acre dApp</h1>
-  return <h1>Acre dApp</h1>
+  return <h1>Ledger live - Acre dApp</h1>
 }
 
 function DAppWrapper() {
-  const { isEmbed } = useEmbedFeatureFlag()
-
-  if (isEmbed === "true") {
-    return (
-      <LedgerWalletAPIProvider>
-        <DApp />
-      </LedgerWalletAPIProvider>
-    )
-  }
-  return <DApp />
+  return (
+    <LedgerWalletAPIProvider>
+      <DApp />
+    </LedgerWalletAPIProvider>
+  )
 }
 
 export default DAppWrapper
