7245eb938df35e565afb50b6fe3041d088cf873c
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-11-17 10:02:16 +0100

    Eslint fixes for dApp

--- dapp/src/theme/utils/utils.ts
+++ dapp/src/theme/utils/utils.ts
@@ -1,7 +1,8 @@
 // For the ledger live app, we should detect the theme set for the user.
 export function getThemeConfig(theme: string | null) {
-    return {
-      initialColorMode: import.meta.env.VITE_SUPPORT_THEME_MODE === "true" ? theme : "light",
-      useSystemColorMode: false,
-    }
+  return {
+    initialColorMode:
+      import.meta.env.VITE_SUPPORT_THEME_MODE === "true" ? theme : "light",
+    useSystemColorMode: false,
   }
+}
