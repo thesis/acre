7a49c6270475d35a71d89ea1f52444b537ea3c9d
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-11-17 10:46:56 +0100

    Delete the env variable `VITE_SUPPORT_THEME_MODE`

--- dapp/src/theme/utils/config.ts
+++
@@ -1,8 +0,0 @@
-// For the ledger live app, we should detect the theme set for the user.
-export function getThemeConfig(theme: string | null) {
-  return {
-    initialColorMode:
-      import.meta.env.VITE_SUPPORT_THEME_MODE === "true" ? theme : "light",
-    useSystemColorMode: false,
-  }
-}
