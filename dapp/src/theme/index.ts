404d3c90d71c4e937c983e012965a2d09d68ba5b
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-11-17 12:19:23 +0100

    Update of global styles for themes

--- dapp/src/theme/index.ts
+++ dapp/src/theme/index.ts
@@ -1,9 +1,19 @@
-import { extendTheme } from "@chakra-ui/react"
+import { StyleFunctionProps, extendTheme } from "@chakra-ui/react"
+import { mode } from "@chakra-ui/theme-tools"
 import Button from "./Button"
 import { colors } from "./utils"
 
 const defaultTheme = {
   colors,
+  styles: {
+    global: (props: StyleFunctionProps) => ({
+      "html, body, #root, #root > div": {
+        backgroundColor: mode("lightGrey", "darkGrey")(props),
+        color: mode("black", "grey.80")(props),
+        minHeight: "100vh",
+      },
+    }),
+  },
   components: {
     Button,
   },
