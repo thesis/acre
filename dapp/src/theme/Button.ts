954bd9e15cc0c649685f9f678b3039835488b20f
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-11-20 11:34:09 +0100

    Update of font styles for buttons

--- dapp/src/theme/Button.ts
+++ dapp/src/theme/Button.ts
@@ -2,6 +2,17 @@ import { mode } from "@chakra-ui/theme-tools"
 import type { StyleFunctionProps } from "@chakra-ui/styled-system"
 
 const Button = {
+  baseStyle: {
+    fontWeight: "500",
+  },
+  sizes: {
+    md: {
+      fontSize: "14px",
+    },
+    lg: {
+      fontSize: "16px",
+    },
+  },
   variants: {
     solid: (props: StyleFunctionProps) => ({
       backgroundColor: mode("black", "purple")(props),
