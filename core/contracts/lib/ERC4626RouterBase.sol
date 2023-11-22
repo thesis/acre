de39aa294ae07a6900c766465f6a2a9020decc44
Dmitry <dymitr.paremski@thesis.co>
2023-11-22 12:54:34 +0100

    Copying ERC4626 related contracts from ERC4626-Alliance
    Only some of the contracts were copied and pasted just for Acre needs.
    In Acre project we use HH with yarn manager and this is why the path to
    solmate's library in node_modules had to be changed. Added only `/src` to the path.
    The rest of the code was unchanged and is based on 643cd04 commit from the
    following repo: https://github.com/ERC4626-Alliance/ERC4626-Contracts

---
+++ core/contracts/lib/ERC4626RouterBase.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.10;
+
+import {IERC4626, IERC4626RouterBase, ERC20} from "./interfaces/IERC4626RouterBase.sol";
+import {SafeTransferLib} from "solmate/src/utils/SafeTransferLib.sol";
+
+import {SelfPermit} from "./external/SelfPermit.sol";
+import {Multicall} from "./external/Multicall.sol";
+import {PeripheryPayments, IWETH9} from "./external/PeripheryPayments.sol";
+
+/// @title ERC4626 Router Base Contract
+abstract contract ERC4626RouterBase is IERC4626RouterBase, SelfPermit, Multicall, PeripheryPayments {
+    using SafeTransferLib for ERC20;
+
+    /// @inheritdoc IERC4626RouterBase
+    function mint(
+        IERC4626 vault,
+        address to,
+        uint256 shares,
+        uint256 maxAmountIn
+    ) public payable virtual override returns (uint256 amountIn) {
+        if ((amountIn = vault.mint(shares, to)) > maxAmountIn) {
+            revert MaxAmountError();
+        }
+    }
+
+    /// @inheritdoc IERC4626RouterBase
+    function deposit(
+        IERC4626 vault,
+        address to,
+        uint256 amount,
+        uint256 minSharesOut
+    ) public payable virtual override returns (uint256 sharesOut) {
+        if ((sharesOut = vault.deposit(amount, to)) < minSharesOut) {
+            revert MinSharesError();
+        }
+    }
+
+    /// @inheritdoc IERC4626RouterBase
+    function withdraw(
+        IERC4626 vault,
+        address to,
+        uint256 amount,
+        uint256 maxSharesOut
+    ) public payable virtual override returns (uint256 sharesOut) {
+        if ((sharesOut = vault.withdraw(amount, to, msg.sender)) > maxSharesOut) {
+            revert MaxSharesError();
+        }
+    }
+
+    /// @inheritdoc IERC4626RouterBase
+    function redeem(
+        IERC4626 vault,
+        address to,
+        uint256 shares,
+        uint256 minAmountOut
+    ) public payable virtual override returns (uint256 amountOut) {
+        if ((amountOut = vault.redeem(shares, to, msg.sender)) < minAmountOut) {
+            revert MinAmountError();
+        }
+    }
+}
