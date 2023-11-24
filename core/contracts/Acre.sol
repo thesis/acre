c81cfd570008b17a75476b07496b85bd3125b4d7
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2023-11-24 16:17:17 +0100

    Add initial implementation of the stake function.
    Stakes a given amount of underlying token and mints shares to a
    receiver. This function calls `deposit` function from `ERC4626`
    contract. The function should accept `referrer` input parameter that will
    be later used for accounting (out of scope of this issue).

--- core/contracts/Acre.sol
+++ core/contracts/Acre.sol
@@ -5,4 +5,21 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
 contract Acre is ERC4626 {
     constructor(IERC20 _token) ERC4626(_token) ERC20("Staking BTC", "stBTC") {}
+
+    /// @notice Stakes a given amount of underlying token and mints shares to a
+    ///         receiver.
+    /// @dev This function calls `deposit` function from `ERC4626` contract.
+    /// @param assets Approved amount for the transfer and stake.
+    /// @param receiver The address to which the shares will be minted.
+    /// @param referrer Data used for refferal program.
+    /// @return shares Minted shares.
+    function stake(
+        uint256 assets,
+        address receiver,
+        bytes32 referrer
+    ) external returns (uint256 shares) {
+        require(referrer != bytes32(0), "Referrer can not be empty");
+
+        return super.deposit(assets, receiver);
+    }
 }
