b7aee18c080069ba7a3bcab90f42a10e01813337
Dmitry <dymitr.paremski@thesis.co>
2023-11-24 23:04:55 +0100

    Drafting allocate and collect functions for Acre Router

--- core/contracts/AcreRouter.sol
+++ core/contracts/AcreRouter.sol
@@ -1,17 +1,16 @@
-pragma solidity ^0.8.10;
+pragma solidity 0.8.20;
 
-import {ERC4626RouterBase} from "./lib/erc4626/ERC4626RouterBase.sol";
-import {PeripheryPayments, IWETH9} from "./lib/erc4626/external/PeripheryPayments.sol";
 import {SafeTransferLib} from "solmate/src/utils/SafeTransferLib.sol";
 import {ERC20} from "solmate/src/tokens/ERC20.sol";
+import {ERC4626} from "solmate/src/mixins/ERC4626.sol";
 import {Owned} from "solmate/src/auth/Owned.sol";
-import {IERC4626} from "./lib/erc4626/interfaces/IERC4626.sol";
 
 // TODO: add description
-contract AcreRouter is ERC4626RouterBase, Owned {
+contract AcreRouter is Owned {
     using SafeTransferLib for ERC20;
 
     ERC20 public immutable stBTC;
+    ERC20 public immutable tBTC;
 
     /// @notice Approved allocators which essentially are the ERC4626 vaults that
     ///         deposit funds to yield strategies, e.g. Uniswap V3 WBTC/TBTC pool.
@@ -19,30 +18,31 @@ contract AcreRouter is ERC4626RouterBase, Owned {
     ///         perspective, the Allocator contract can be a part of an external
     ///         Yield Module and does not care how the yield is generated.
     address[] public allocators;
-
     mapping(address => bool) public isAllocator;
 
-    /// @notice Indicates if the given address is an Acre Manager. Only Acre Manager
-    ///         can set or remove allocators.
+    /// @notice Acre Manager address. Only Acre Manager can set or remove
+    ///         Strategy Allocators.
     address public acreManager;
 
     event AllocatorAdded(address indexed allocator);
     event AllocatorRemoved(address indexed allocator);
+
+    event AcreManagerSet(address indexed manager);
 
     modifier onlyAcreManager() {
         require(msg.sender == acreManager, "Caller is not an Acre Manager");
         _;
     }
 
-    constructor(
-        IWETH9 weth,
-        ERC20 _stBTC
-    ) Owned(msg.sender) PeripheryPayments(weth) {
+    constructor(ERC20 _stBTC, ERC20 _tBTC) Owned(msg.sender) {
         stBTC = _stBTC;
+        tBTC = _tBTC;
     }
 
-    function setAcreManager(address _acreManager) external onlyOwner {
-        acreManager = _acreManager;
+    function setAcreManager(address manager) external onlyOwner {
+        require(manager != address(0), "Zero address");
+        acreManager = manager;
+        emit AcreManagerSet(manager);
     }
 
     function addAllocator(address allocator) external onlyAcreManager {
@@ -53,14 +53,13 @@ contract AcreRouter is ERC4626RouterBase, Owned {
     }
 
     function removeAllocator(address allocator) external onlyAcreManager {
-        require(isAllocator[allocator], "This address is not a minter");
+        require(isAllocator[allocator], "Not an allocator");
 
         delete isAllocator[allocator];
 
         for (uint256 i = 0; i < allocators.length; i++) {
             if (allocators[i] == allocator) {
                 allocators[i] = allocators[allocators.length - 1];
-                // slither-disable-next-line costly-loop
                 allocators.pop();
                 break;
             }
@@ -69,25 +68,38 @@ contract AcreRouter is ERC4626RouterBase, Owned {
         emit AllocatorRemoved(allocator);
     }
 
-    /// @notice Allocates funds from stBTC (Acre) to an allocator
+    /// @notice Routes funds from stBTC (Acre) to a given allocator
+    /// @param allocator Address of the allocator to route the funds to.
+    /// @param amount Amount of TBTC to allocate.
     function allocate(address allocator, uint256 amount) public {
-        require(msg.sender == address(stBTC), "stBTC must be a caller");
-
+        require(msg.sender == address(stBTC), "stBTC only");
         if (!isAllocator[allocator]) {
             revert("Allocator is not approved");
         }
 
-        // TODO: implement allocation logic to an allocator
+        tBTC.safeTransferFrom(msg.sender, address(this), amount);
+        tBTC.safeApprove(allocator, amount);
+        // TODO: implement protection from the inflation attack / slippage
+        ERC4626(allocator).deposit(amount, address(this));
     }
 
-    /// @notice Collects funds from an allocator and sends them to stBTC (Acre)
-    function collect(address allocator, uint256 amount) public {
-        require(msg.sender == address(stBTC), "stBTC must be a caller");
+    /// @notice Collects TBTC from an allocator and approves them to be collected
+    ///         by stBTC (Acre)
+    /// @param allocator Address of the allocator to collect the assets from.
+    /// @param shares Amount of shares to collect. Shares are the internal representation
+    ///               of the underlying asset in the allocator. Concrete amount of the
+    ///               underlying asset is calculated by calling `convertToAssets` on
+    ///               the allocator and the shares are burned.
+    function collect(address allocator, uint256 shares) public {
+        require(msg.sender == address(stBTC), "stBTC only");
 
         if (!isAllocator[allocator]) {
             revert("Allocator is not approved");
         }
 
-        // TODO: implement collection logic from an allocator
+        // TODO: implement protection from the inflation attack / slippage
+        // TODO: use IERC4626 interface
+        uint256 assets = ERC4626(allocator).redeem(shares, address(this), address(this));
+        tBTC.safeApprove(address(stBTC), assets);
     }
 }
