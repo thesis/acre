3ebb88b5567e8019c0079ca1a1fd7e2e3f7b17da
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2023-11-24 15:25:51 +0100

    Initial implementation of the main Acre contract
    The Acre contract is ERC4626 tokenized vault standard. Here we inherit
    from abstract contract `ERC4626` from OpenZeppelin lib. We are going to
    implement some custom features in follow-up work but the core is based
    on the ERC4626 standard.

--- core/contracts/Acre.sol
+++ core/contracts/Acre.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.21;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
-contract Acre {
-    // TODO: add your implementation
+contract Acre is ERC4626 {
+    constructor(IERC20 _token) ERC4626(_token) ERC20("Staking BTC", "stBTC") {}
 }
