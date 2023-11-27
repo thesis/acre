f36c65cd38ff7ba6a2e04944671f4d4523c294aa
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2023-11-27 11:35:38 +0100

    Add support for receive approval pattern to Acre
    The tBTC token contract that will be a staking token supports this
    pattern. To be able to stake in one transaction (instead of 2: approve +
    stake) we must implement the `RecieveApproval` interface. The token
    staking contract receives approval to spend tokens and create a stake
    for a given account.

--- core/contracts/Acre.sol
+++ core/contracts/Acre.sol
@@ -2,9 +2,33 @@
 pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import "@thesis/solidity-contracts/contracts/token/IReceiveApproval.sol";
 
-contract Acre is ERC4626 {
+contract Acre is ERC4626, IReceiveApproval {
     constructor(IERC20 _token) ERC4626(_token) ERC20("Staking BTC", "stBTC") {}
+
+    /// @notice Receives approval of token transfer and stakes the approved
+    ///         amount.
+    /// @dev Requires that the provided token contract be the same one linked to
+    ///      this contract.
+    /// @param from The owner of the tokens who approved them to transfer.
+    /// @param amount Approved amount for the transfer and stake.
+    /// @param _token Token contract address.
+    /// @param extraData Extra data for stake. This byte array must have the
+    ///        following values concatenated:
+    ///        - referrer ID (32 bytes)
+    function receiveApproval(
+        address from,
+        uint256 amount,
+        address _token,
+        bytes calldata extraData
+    ) external override {
+        require(_token == super.asset(), "Unrecognized token");
+        // TODO: Decide on the format of the `extradata` variable.
+        require(extraData.length >= 32, "Corrupted stake data");
+
+        this.stake(amount, from, bytes32(extraData));
+    }
 
     /// @notice Stakes a given amount of underlying token and mints shares to a
     ///         receiver.
