6837e74915b3647bbe37758a1159fcd4b1a5e07e
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2023-11-30 12:23:17 +0100

    Revert "Add support for receive approval pattern to Acre"
    This reverts commit f36c65cd38ff7ba6a2e04944671f4d4523c294aa.
    
    To support receive approval pattern we need to override the
    `ERC4626.deposit` function to allow pass an `owner`. The OZ
    implementation sets the owner as `msg.sender` by default under the hood
    see
    https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC4626.sol#L178
    and we can't change the owner. W/o this update using approve and call
    pattern to stake tokens, the `msg.sender` is tBTC token address, so we
    are actually trying to send tokens from the token contract to the
    receiver, which is impossible. We are going to address it in a separate
    PR.

--- core/contracts/Acre.sol
+++ core/contracts/Acre.sol
@@ -2,37 +2,13 @@
 pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import "@thesis/solidity-contracts/contracts/token/IReceiveApproval.sol";
 
-contract Acre is ERC4626, IReceiveApproval {
+contract Acre is ERC4626 {
     event Staked(bytes32 indexed referral, uint256 assets, uint256 shares);
 
     constructor(
         IERC20 _token
     ) ERC4626(_token) ERC20("Acre Staked Bitcoin", "stBTC") {}
-
-    /// @notice Receives approval of token transfer and stakes the approved
-    ///         amount.
-    /// @dev Requires that the provided token contract be the same one linked to
-    ///      this contract.
-    /// @param from The owner of the tokens who approved them to transfer.
-    /// @param amount Approved amount for the transfer and stake.
-    /// @param token Token contract address.
-    /// @param extraData Extra data for stake. This byte array must have the
-    ///        following values concatenated:
-    ///        - referral (32 bytes)
-    function receiveApproval(
-        address from,
-        uint256 amount,
-        address token,
-        bytes calldata extraData
-    ) external override {
-        require(token == asset(), "Unrecognized token");
-        // TODO: Decide on the format of the `extradata` variable.
-        require(extraData.length == 32, "Corrupted stake data");
-
-        stake(amount, from, bytes32(extraData));
-    }
 
     /// @notice Stakes a given amount of underlying token and mints shares to a
     ///         receiver.
