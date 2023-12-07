9a9c5137224e4450f1854c83e787db63314778b3
Dmitry <dymitr.paremski@thesis.co>
2023-12-07 15:34:39 +0100

    Adding AcreRouter to deployed context

--- core/test/helpers/context.ts
+++ core/test/helpers/context.ts
@@ -2,7 +2,7 @@ import { deployments } from "hardhat"
 
 import { getDeployedContract } from "./contract"
 
-import type { Acre, TestERC20 } from "../../typechain"
+import type { Acre, AcreRouter, TestERC20 } from "../../typechain"
 
 // eslint-disable-next-line import/prefer-default-export
 export async function deployment() {
@@ -10,6 +10,7 @@ export async function deployment() {
 
   const tbtc: TestERC20 = await getDeployedContract("TBTC")
   const acre: Acre = await getDeployedContract("Acre")
+  const acreRouter: AcreRouter = await getDeployedContract("AcreRouter")
 
-  return { tbtc, acre }
+  return { tbtc, acre, acreRouter }
 }
