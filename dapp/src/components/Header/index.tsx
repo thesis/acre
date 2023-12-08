b10ca854028bbf4a9fee434748bb35f725a452a0
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-12-08 10:42:17 +0100

    Set the page header for dApp

--- dapp/src/components/Header/index.tsx
+++ dapp/src/components/Header/index.tsx
@@ -1,11 +1,15 @@
 import React from "react"
-import { Flex } from "@chakra-ui/react"
+import { Box, Flex, Icon } from "@chakra-ui/react"
 import ConnectWallet from "./ConnectWallet"
+import { AcreLogo } from "../../static/icons"
 
 export default function Header() {
   return (
-    <Flex justifyContent="end" p={6}>
-      <ConnectWallet />
-    </Flex>
+    <Box as="header">
+      <Icon as={AcreLogo} boxSize={20} position="absolute" left={6} />
+      <Flex justifyContent="end" p={6}>
+        <ConnectWallet />
+      </Flex>
+    </Box>
   )
 }
