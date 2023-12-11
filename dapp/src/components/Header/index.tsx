21dc2ffd6d72dd881f79529a759d0a58175039d7
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-12-11 10:30:49 +0100

    Simplify the `Header` component

--- dapp/src/components/Header/index.tsx
+++ dapp/src/components/Header/index.tsx
@@ -1,15 +1,15 @@
 import React from "react"
-import { Box, Flex, Icon } from "@chakra-ui/react"
+import { Flex, HStack, Icon } from "@chakra-ui/react"
 import ConnectWallet from "./ConnectWallet"
 import { AcreLogo } from "../../static/icons"
 
 export default function Header() {
   return (
-    <Box as="header">
-      <Icon as={AcreLogo} boxSize={20} position="absolute" left={6} />
-      <Flex justifyContent="end" p={6}>
+    <HStack as="header" p={6}>
+      <Icon as={AcreLogo} boxSize={20} />
+      <Flex ml="auto">
         <ConnectWallet />
       </Flex>
-    </Box>
+    </HStack>
   )
 }
