5322d63723d6773772acda6f8d3662b157aafb49
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-12-12 16:32:07 +0100

    Use `format("opentype")` instead of `format("otf")`

--- dapp/src/components/GlobalStyles/index.tsx
+++ dapp/src/components/GlobalStyles/index.tsx
@@ -13,31 +13,31 @@ export default function GlobalStyles() {
       styles={`
         @font-face {
           font-family: "Segment";
-          src: url(${SegmentRegular}) format("otf");
+          src: url(${SegmentRegular}) format("opentype");
           font-weight: 400;
           font-style: normal;
         }
         @font-face {
           font-family: "Segment";
-          src: url(${SegmentMedium}) format("otf");
+          src: url(${SegmentMedium}) format("opentype");
           font-weight: 500;
           font-style: normal;
         }
         @font-face {
           font-family: "Segment";
-          src: url(${SegmentSemiBold}) format("otf");
+          src: url(${SegmentSemiBold}) format("opentype");
           font-weight: 600;
           font-style: normal;
         }
         @font-face {
           font-family: "Segment";
-          src: url(${SegmentBold}) format("otf");
+          src: url(${SegmentBold}) format("opentype");
           font-weight: 700;
           font-style: normal;
         }
         @font-face {
           font-family: "Segment";
-          src: url(${SegmentBlack}) format("otf");
+          src: url(${SegmentBlack}) format("opentype");
           font-weight: 900;
           font-style: normal;
         }
