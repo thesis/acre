50adea6f90780b206c1cc83d7f211598e883f36c
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-12-13 12:19:46 +0100

    Update a comments

--- dapp/src/theme/Tooltip.ts
+++ dapp/src/theme/Tooltip.ts
@@ -4,12 +4,14 @@ import {
   cssVar,
 } from "@chakra-ui/react"
 
-// Currently, there is no possibility to set all tooltips with hasArrow by defaultProps.
-// Let's override the defaultProps as follows.
+// Currently, there is no possibility to set all tooltips with hasArrow by
+// defaultProps. Let's override the defaultProps as follows.
 ChakraTooltip.defaultProps = { ...ChakraTooltip.defaultProps, hasArrow: true }
 
-// To make the arrow the same color as the background, the css variable needs to be set correctly.
-// More info: https://github.com/chakra-ui/chakra-ui/issues/4695#issuecomment-991023319
+// To make the arrow the same color as the background, the css variable needs to
+// be set correctly.
+// More info:
+// https://github.com/chakra-ui/chakra-ui/issues/4695#issuecomment-991023319
 const $arrowBg = cssVar("popper-arrow-bg")
 
 const Tooltip: ComponentSingleStyleConfig = {
