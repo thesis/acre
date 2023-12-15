ba3298bdc957dda91265180d7345b5034576737d
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2023-12-15 09:15:05 +0100

    Simplify style definitions

--- dapp/src/theme/Drawer.ts
+++ dapp/src/theme/Drawer.ts
@@ -1,24 +1,34 @@
-import { ComponentSingleStyleConfig } from "@chakra-ui/react"
+import { drawerAnatomy as parts } from "@chakra-ui/anatomy"
+import { createMultiStyleConfigHelpers, defineStyle } from "@chakra-ui/react"
 
-const Drawer: ComponentSingleStyleConfig = {
-  baseStyle: {
-    dialogContainer: {
-      zIndex: "drawer",
-    },
-    overlay: {
-      bg: "none",
-      backdropFilter: "auto",
-      backdropBlur: "8px",
-    },
-    dialog: {
-      borderTop: "2px",
-      borderLeft: "2px",
-      boxShadow: "none",
-      borderColor: "white",
-      borderTopLeftRadius: "xl",
-      bg: "gold.100",
-    },
-  },
-}
+const { defineMultiStyleConfig, definePartsStyle } =
+  createMultiStyleConfigHelpers(parts.keys)
+
+const baseStyleDialogContainer = defineStyle({
+  zIndex: "drawer",
+})
+
+const baseStyleDialog = defineStyle({
+  borderTop: "2px",
+  borderLeft: "2px",
+  boxShadow: "none",
+  borderColor: "white",
+  borderTopLeftRadius: "xl",
+  bg: "gold.100",
+})
+
+const baseStyleOverlay = defineStyle({
+  bg: "none",
+  backdropFilter: "auto",
+  backdropBlur: "8px",
+})
+
+const baseStyle = definePartsStyle({
+  dialogContainer: baseStyleDialogContainer,
+  dialog: baseStyleDialog,
+  overlay: baseStyleOverlay,
+})
+
+const Drawer = defineMultiStyleConfig({ baseStyle })
 
 export default Drawer
--- dapp/src/theme/Sidebar.ts
+++ dapp/src/theme/Sidebar.ts
@@ -1,27 +1,35 @@
-import { ComponentMultiStyleConfig } from "@chakra-ui/react"
+import { createMultiStyleConfigHelpers, defineStyle } from "@chakra-ui/react"
 
-const Sidebar: ComponentMultiStyleConfig = {
-  parts: ["sidebarContainer", "sidebar"],
-  baseStyle: {
-    sidebarContainer: {
-      top: 0,
-      right: 0,
-      h: "100vh",
-      position: "fixed",
-      overflow: "hidden",
-      zIndex: "sidebar",
-      transition: "width 0.3s",
-    },
-    sidebar: {
-      p: 4,
-      height: "100%",
-      bg: "gold.200",
-      borderTop: "2px",
-      borderLeft: "2px",
-      borderColor: "gold.100",
-      borderTopLeftRadius: "xl",
-    },
-  },
-}
+const PARTS = ["sidebarContainer", "sidebar"]
+
+const { defineMultiStyleConfig, definePartsStyle } =
+  createMultiStyleConfigHelpers(PARTS)
+
+const baseStyleSidebarContainer = defineStyle({
+  top: 0,
+  right: 0,
+  h: "100vh",
+  position: "fixed",
+  overflow: "hidden",
+  zIndex: "sidebar",
+  transition: "width 0.3s",
+})
+
+const baseStyleSidebar = defineStyle({
+  p: 4,
+  height: "100%",
+  bg: "gold.200",
+  borderTop: "2px",
+  borderLeft: "2px",
+  borderColor: "gold.100",
+  borderTopLeftRadius: "xl",
+})
+
+const baseStyle = definePartsStyle({
+  sidebarContainer: baseStyleSidebarContainer,
+  sidebar: baseStyleSidebar,
+})
+
+const Sidebar = defineMultiStyleConfig({ baseStyle })
 
 export default Sidebar
