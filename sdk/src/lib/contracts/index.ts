c84ba51781798d981a3a6dbfdc7a073d0645528d
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-01-01 12:41:48 +0100

    Create `TBTCDepositor` contract interface in SDK

--- sdk/src/lib/contracts/index.ts
+++ sdk/src/lib/contracts/index.ts
@@ -1,1 +1,8 @@
+import { TBTCDepositor } from "./tbtc-depositor"
+
+export * from "./tbtc-depositor"
 export * from "./chain-identifier"
+
+export type AcreContracts = {
+  depositor: TBTCDepositor
+}
