fe8ac88935ae5d59f83d18c7e115d5b39173f30a
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-01-03 13:08:44 +0100

    Prepare custom action hooks for staking steps

--- dapp/src/components/Modals/Staking/DepositBTC.tsx
+++ dapp/src/components/Modals/Staking/DepositBTC.tsx
@@ -1,26 +1,19 @@
 import React from "react"
 import Alert from "../../shared/Alert"
-import { useModalFlowContext, useWalletContext } from "../../../hooks"
+import { useDepositBTCTransaction, useModalFlowContext } from "../../../hooks"
 import StakeSteps from "./StakeSteps"
 import { TextMd } from "../../shared/Typography"
 
 export default function DepositBTC() {
   const { goNext } = useModalFlowContext()
-  const { btcAccount } = useWalletContext()
-
-  const handleSendBTC = async () => {
-    if (!btcAccount?.id) return
-
-    // TODO: Send the correct transaction
-    goNext()
-  }
+  const { depositBTC } = useDepositBTCTransaction(goNext)
 
   return (
     <StakeSteps
       header="Step 2 / 2"
       buttonText="Deposit BTC"
       activeStep={1}
-      onClick={handleSendBTC}
+      onClick={depositBTC}
     >
       <Alert status="info" withIcon>
         <TextMd>
--- dapp/src/components/Modals/Staking/SignMessage.tsx
+++ dapp/src/components/Modals/Staking/SignMessage.tsx
@@ -1,36 +1,20 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { Highlight } from "@chakra-ui/react"
-import { useSignMessage } from "@ledgerhq/wallet-api-client-react"
 import Alert from "../../shared/Alert"
-import { useModalFlowContext, useWalletContext } from "../../../hooks"
+import { useModalFlowContext, useSignMessage } from "../../../hooks"
 import StakeSteps from "./StakeSteps"
 import { TextMd } from "../../shared/Typography"
 
-const SIGN_MESSAGE = "Test message"
-
 export default function SignMessage() {
   const { goNext } = useModalFlowContext()
-  const { ethAccount } = useWalletContext()
-  const { signMessage, signature } = useSignMessage()
-
-  const handleSignMessage = async () => {
-    if (!ethAccount?.id) return
-
-    await signMessage(ethAccount.id, Buffer.from(SIGN_MESSAGE, "utf-8"))
-  }
-
-  useEffect(() => {
-    if (signature) {
-      goNext()
-    }
-  }, [goNext, signature])
+  const { signMessage } = useSignMessage(goNext)
 
   return (
     <StakeSteps
       header="Step 1 / 2"
       buttonText="Continue"
       activeStep={0}
-      onClick={handleSignMessage}
+      onClick={signMessage}
     >
       <Alert status="info" withIcon>
         <TextMd>
