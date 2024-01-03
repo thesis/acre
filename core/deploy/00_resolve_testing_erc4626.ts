a59d6f43f710c1f9ee96bb147450c800fd023f40
Dmitry <dymitr.paremski@thesis.co>
2024-01-03 11:40:24 +0100

    Removing allowStubs tag check. Not needed.

--- core/deploy/00_resolve_testing_erc4626.ts
+++ core/deploy/00_resolve_testing_erc4626.ts
@@ -5,20 +5,17 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { getNamedAccounts, deployments } = hre
   const { log } = deployments
   const { deployer } = await getNamedAccounts()
-
   const tBTC = await deployments.get("TBTC")
 
-  if (hre.network.tags.allowStubs) {
-    log("deploying Mock ERC4626 Vault")
+  log("deploying Mock ERC4626 Vault")
 
-    await deployments.deploy("Vault", {
-      contract: "TestERC4626",
-      from: deployer,
-      args: [tBTC.address, "MockVault", "MV"],
-      log: true,
-      waitConfirmations: 1,
-    })
-  }
+  await deployments.deploy("Vault", {
+    contract: "TestERC4626",
+    from: deployer,
+    args: [tBTC.address, "MockVault", "MV"],
+    log: true,
+    waitConfirmations: 1,
+  })
 }
 
 export default func
