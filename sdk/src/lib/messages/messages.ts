181b2661f23e061b08b691b44a67712026dabda8
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-01-04 13:45:25 +0100

    Update chain messages interface
    Add `ChainSignedMessage` that represents a signature. This interface
    exposes `verify` function that returns an address of the private key
    that produced the signature. Also here we update the `ChainMessages`
    interface - `sign` function should return the `ChainSignedMessage`
    object.

--- sdk/src/lib/messages/messages.ts
+++ sdk/src/lib/messages/messages.ts
@@ -1,15 +1,18 @@
+import { ChainIdentifier } from "../contracts"
 import { Hex } from "../utils"
 
-export type SignedMessage = {
-  v: 0 | 1 | 27 | 28
-  r: Hex
-  s: Hex
-  signature: Hex
+export type V = 0 | 1 | 27 | 28
+export interface ChainSignedMessage {
+  readonly v: V
+  readonly r: Hex
+  readonly s: Hex
+  readonly signature: Hex
+  verify(): ChainIdentifier
 }
 
 // The main idea is to hide the signing process under the exact implementation
 // for a given chain. For example, we can use `ethers` lib for Ethereum
 // implementation, but different lib for other chains.
 export interface ChainMessages {
-  sign(message: string): Promise<SignedMessage>
+  sign(message: string): Promise<ChainSignedMessage>
 }
