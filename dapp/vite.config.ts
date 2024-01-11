5e9248b1b22be032f011fa9eb45c01c39aa55242
ioay <ayioerthi@gmail.com>
2024-01-11 14:17:34 +0100

    Update config files

--- dapp/vite.config.ts
+++ dapp/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { nodePolyfills } from "vite-plugin-node-polyfills"
+import eslint from "vite-plugin-eslint"
+
+import { resolve } from "path"
 
 export default defineConfig({
-  plugins: [nodePolyfills(), react()],
+  resolve: {
+    alias: {
+      "~": resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [nodePolyfills(), react(), eslint()],
 })
