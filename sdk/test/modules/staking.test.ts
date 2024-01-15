08b0872ee4af2d123579d20ddd297e26ec4802e0
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-01-15 17:38:00 +0100

    Add unit test for staking module
    Clean up tests by extracting test data to variables outside of the tests
    brackets. Add unit test for `StakeInitialization` object which is
    created by `StakingModule.initializeStake`.

--- sdk/test/modules/staking.test.ts
+++ sdk/test/modules/staking.test.ts
@@ -1,10 +1,80 @@
-import { EthereumAddress } from "@keep-network/tbtc-v2.ts"
+import TBTCModule, {
+  BitcoinRawTx,
+  BitcoinRawTxVectors,
+  BitcoinTxHash,
+  BitcoinUtxo,
+  DepositReceipt,
+  EthereumAddress,
+} from "@keep-network/tbtc-v2.ts"
+import { ethers } from "ethers"
 import { AcreContracts } from "../../src/lib/contracts"
 import { StakingModule } from "../../src/modules/staking"
+import { Hex } from "../../src/lib/utils"
 import { MockAcreContracts } from "../utils/mock-acre-contracts"
 import { MockMessageSigner } from "../utils/mock-message-signer"
 import { MockTBTC } from "../utils/mock-tbtc"
 import { StakeInitialization } from "../../src/modules/staking/stake-initialization"
+
+jest.mock("@keep-network/tbtc-v2.ts", () => ({
+  extractBitcoinRawTxVectors: jest.fn(),
+  ...jest.requireActual("@keep-network/tbtc-v2.ts"),
+}))
+
+const stakingModuleData: {
+  initializeStake: {
+    receiver: EthereumAddress
+    referral: number
+    bitcoinRecoveryAddress: string
+    mockedDepositBTCAddress: string
+  }
+} = {
+  initializeStake: {
+    receiver: EthereumAddress.from(ethers.Wallet.createRandom().address),
+    referral: 1,
+    bitcoinRecoveryAddress: "mjc2zGWypwpNyDi4ZxGbBNnUA84bfgiwYc",
+    mockedDepositBTCAddress:
+      "tb1qma629cu92skg0t86lftyaf9uflzwhp7jk63h6mpmv3ezh6puvdhs6w2r05",
+  },
+}
+
+const stakingInitializationData: {
+  fundingUtxos: Array<Omit<BitcoinUtxo, "value"> & { value: bigint }>
+  bitcoinRawTx: BitcoinRawTx
+  bitcoinRawTxVectors: BitcoinRawTxVectors
+  depositReceipt: Omit<DepositReceipt, "depositor">
+  mockedInitializeTxHash: Hex
+} = {
+  fundingUtxos: [
+    {
+      transactionHash: BitcoinTxHash.from(
+        "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+      ),
+      outputIndex: 1,
+      value: 2222n,
+    },
+    {
+      transactionHash: BitcoinTxHash.from(
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+      ),
+      outputIndex: 0,
+      value: 1111n,
+    },
+  ],
+  bitcoinRawTx: { transactionHex: "" },
+  bitcoinRawTxVectors: {
+    version: Hex.from("111111"),
+    inputs: Hex.from("222222"),
+    outputs: Hex.from("333333"),
+    locktime: Hex.from("444444"),
+  },
+  depositReceipt: {
+    blindingFactor: Hex.from("555555"),
+    walletPublicKeyHash: Hex.from("666666"),
+    refundPublicKeyHash: Hex.from("777777"),
+    refundLocktime: Hex.from("888888"),
+  },
+  mockedInitializeTxHash: Hex.from("999999"),
+}
 
 describe("Staking", () => {
   const contracts: AcreContracts = new MockAcreContracts()
@@ -18,24 +88,29 @@ describe("Staking", () => {
   )
 
   describe("initializeStake", () => {
-    let result: StakeInitialization
-    const bitcoinRecoveryAddress = "mjc2zGWypwpNyDi4ZxGbBNnUA84bfgiwYc"
-    const mockedDepositBTCAddress =
-      "tb1qma629cu92skg0t86lftyaf9uflzwhp7jk63h6mpmv3ezh6puvdhs6w2r05"
+    const {
+      mockedDepositBTCAddress,
+      bitcoinRecoveryAddress,
+      receiver,
+      referral,
+    } = stakingModuleData.initializeStake
     const mockedDeposit = {
       getBitcoinAddress: jest.fn().mockResolvedValue(mockedDepositBTCAddress),
+      detectFunding: jest.fn(),
+      getReceipt: jest.fn(),
     }
     const mockedSignedMessage = { verify: jest.fn() }
+    let result: StakeInitialization
 
-    beforeAll(async () => {
+    beforeEach(async () => {
       tbtc.deposits.initiateDeposit = jest.fn().mockReturnValue(mockedDeposit)
       messageSigner.sign = jest.fn().mockResolvedValue(mockedSignedMessage)
 
-      result = await staking.initializeStake(bitcoinRecoveryAddress)
-    })
-
-    it("should sign message", () => {
-      expect(messageSigner.sign).toHaveBeenCalledWith("message")
+      result = await staking.initializeStake(
+        bitcoinRecoveryAddress,
+        receiver,
+        referral,
+      )
     })
 
     it("should initiate tBTC deposit", () => {
@@ -46,35 +121,175 @@ describe("Staking", () => {
 
     it("should return stake initialization object", async () => {
       expect(result).toBeInstanceOf(StakeInitialization)
-      expect(result.getDepositAddress).toBeDefined()
+      expect(result.getBitcoinAddress).toBeDefined()
       expect(result.stake).toBeDefined()
+      expect(result.signMessage).toBeDefined()
     })
 
     describe("StakeInitialization", () => {
       describe("getDepositAddress", () => {
         it("should return bitcoin deposit address", async () => {
-          expect(await result.getDepositAddress()).toBe(mockedDepositBTCAddress)
+          expect(await result.getBitcoinAddress()).toBe(mockedDepositBTCAddress)
+        })
+      })
+
+      describe("signMessage", () => {
+        describe("when signing by valid receiver", () => {
+          const depositorAddress = ethers.Wallet.createRandom().address
+
+          beforeEach(async () => {
+            mockedSignedMessage.verify.mockReturnValue(receiver)
+            contracts.depositor.getChainIdentifier = jest
+              .fn()
+              .mockReturnValue(EthereumAddress.from(depositorAddress))
+
+            await result.signMessage()
+          })
+
+          it("should sign message", () => {
+            expect(messageSigner.sign).toHaveBeenCalledWith(
+              {
+                name: "TBTCDepositor",
+                version: "1",
+                chainId: 1,
+                verifyingContract: contracts.depositor.getChainIdentifier(),
+              },
+              {
+                Stake: [{ name: "receiver", type: "address" }],
+              },
+              {
+                receiver: receiver.identifierHex,
+              },
+            )
+          })
+
+          it("should verify signed message", () => {
+            expect(mockedSignedMessage.verify).toHaveBeenCalled()
+          })
+        })
+
+        describe("when signing by invalid receiver", () => {
+          const invalidReceiver = EthereumAddress.from(
+            ethers.Wallet.createRandom().address,
+          )
+
+          beforeEach(async () => {
+            mockedSignedMessage.verify = jest
+              .fn()
+              .mockResolvedValue(invalidReceiver)
+          })
+
+          it("should throw an error", () => {
+            expect(async () => {
+              await result.signMessage()
+            }).rejects.toThrow("Invalid receiver address")
+          })
         })
       })
 
       describe("stake", () => {
-        const receiver = EthereumAddress.from(
-          "0x086813525A7dC7dafFf015Cdf03896Fd276eab60",
-        )
-
         beforeAll(() => {
           mockedSignedMessage.verify.mockReturnValue(receiver)
         })
 
-        describe("when the receiver address doesn't match the address recovered from signature", () => {
-          const invalidReceiver = EthereumAddress.from(
-            "0x407C3329eA8f6BEFB984D97AE4Fa71945E43170b",
-          )
-
+        describe("when the message has not been signed yet", () => {
           it("should throw an error", () => {
             expect(async () => {
-              await result.stake(invalidReceiver)
-            }).rejects.toThrow("Invalid receiver address")
+              await result.stake()
+            }).rejects.toThrow("Sign message first")
+          })
+        })
+
+        describe("when message has already been signed", () => {
+          beforeEach(async () => {
+            await result.signMessage()
+          })
+
+          describe("when bitcoin deposit address doesn't have utxos yet", () => {
+            beforeEach(() => {
+              mockedDeposit.detectFunding.mockResolvedValue([])
+            })
+
+            it("should throw an error", () => {
+              expect(async () => {
+                await result.stake()
+              }).rejects.toThrow("Deposit not found yet")
+            })
+          })
+
+          describe("when bitcoin deposit address has utxos", () => {
+            const {
+              fundingUtxos,
+              depositReceipt,
+              bitcoinRawTx,
+              bitcoinRawTxVectors,
+              mockedInitializeTxHash: mockedTxHash,
+            } = stakingInitializationData
+            let spyOnExtractBitcoinRawTxVectors: jest.SpyInstance<TBTCModule.BitcoinRawTxVectors>
+            let tx: Hex
+
+            beforeAll(async () => {
+              mockedDeposit.detectFunding.mockResolvedValue(fundingUtxos)
+              mockedDeposit.getReceipt.mockReturnValue(depositReceipt)
+              spyOnExtractBitcoinRawTxVectors = jest
+                .spyOn(TBTCModule, "extractBitcoinRawTxVectors")
+                .mockReturnValue(bitcoinRawTxVectors)
+
+              tbtc.bitcoinClient.getRawTransaction = jest
+                .fn()
+                .mockResolvedValue(bitcoinRawTx)
+
+              contracts.depositor.initializeStake = jest
+                .fn()
+                .mockResolvedValue(mockedTxHash)
+
+              tx = await result.stake()
+            })
+
+            it("should look for deposits", () => {
+              expect(mockedDeposit.detectFunding).toHaveBeenCalled()
+            })
+
+            it("should get raw transaction for the most recent utxo", () => {
+              const txHash = fundingUtxos[0].transactionHash
+
+              expect(tbtc.bitcoinClient.getRawTransaction).toHaveBeenCalledWith(
+                txHash,
+              )
+            })
+
+            it("should extract bitcoin raw tx vectors", () => {
+              expect(spyOnExtractBitcoinRawTxVectors).toHaveBeenCalledWith(
+                bitcoinRawTx,
+              )
+            })
+
+            it("should get deposit receipt", () => {
+              expect(mockedDeposit.getReceipt).toHaveBeenCalled()
+            })
+
+            it("should initialize stake in tBTC depositor contract", () => {
+              const { outputIndex } = fundingUtxos[0]
+
+              const revealInfo = {
+                fundingOutputIndex: outputIndex,
+                blindingFactor: depositReceipt.blindingFactor,
+                walletPublicKeyHash: depositReceipt.walletPublicKeyHash,
+                refundPublicKeyHash: depositReceipt.refundPublicKeyHash,
+                refundLocktime: depositReceipt.refundLocktime,
+              }
+
+              expect(contracts.depositor.initializeStake).toHaveBeenCalledWith(
+                bitcoinRawTxVectors,
+                revealInfo,
+                receiver,
+                referral,
+              )
+            })
+
+            it("should return transaction hash", () => {
+              expect(tx).toBe(mockedTxHash)
+            })
           })
         })
       })
--- sdk/test/utils/mock-message-signer.ts
+++ sdk/test/utils/mock-message-signer.ts
@@ -1,18 +1,21 @@
 import {
-  ChainMessageSigner,
+  ChainEIP712Signer,
   ChainSignedMessage,
+  Domain,
+  Message,
+  Types,
 } from "../../src/lib/message-signer"
 
 // eslint-disable-next-line import/prefer-default-export
-export class MockMessageSigner implements ChainMessageSigner {
-  constructor() {
-    this.sign = jest.fn()
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars, class-methods-use-this
-  sign(message: string): Promise<ChainSignedMessage> {
+export class MockMessageSigner implements ChainEIP712Signer {
+  // eslint-disable-next-line class-methods-use-this
+  sign(
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    domain: Domain,
+    types: Types,
+    message: Message,
+    /* eslint-enable */
+  ): Promise<ChainSignedMessage> {
     throw new Error("Method not implemented.")
   }
 }
-
-MockMessageSigner.prototype.sign = jest.fn()
