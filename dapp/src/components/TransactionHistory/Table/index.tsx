726d77d0c91e8f952ac1174478ecc052b0a95935
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-01-15 09:29:22 +0100

    Add a pagination to transaction table

--- dapp/src/components/TransactionHistory/Table/index.tsx
+++ dapp/src/components/TransactionHistory/Table/index.tsx
@@ -7,11 +7,15 @@ import {
   Th,
   Td,
   TableContainer,
+  HStack,
+  IconButton,
 } from "@chakra-ui/react"
 import { flexRender } from "@tanstack/react-table"
 import { useTransactionHistoryTable } from "#/hooks"
 import { StakeHistory } from "#/types"
+import { TextSm } from "#/components/shared/Typography"
 import { COLUMNS } from "./columns"
+import { PAGINATION_BUTTONS } from "./pagination"
 
 function Table({ data }: { data: StakeHistory[] }) {
   const table = useTransactionHistoryTable({
@@ -20,38 +24,57 @@ function Table({ data }: { data: StakeHistory[] }) {
   })
 
   return (
-    <TableContainer>
-      <ChakraTable variant="double-row">
-        <Thead>
-          {table.getHeaderGroups().map(({ id, headers }) => (
-            <Tr key={id}>
-              {headers.map((header) => (
-                <Th
-                  key={header.id}
-                  onClick={header.column.getToggleSortingHandler()}
-                >
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext(),
-                  )}
-                </Th>
-              ))}
-            </Tr>
-          ))}
-        </Thead>
-        <Tbody>
-          {table.getRowModel().rows.map((row) => (
-            <Tr key={row.id}>
-              {row.getVisibleCells().map(({ id, column, getContext }) => (
-                <Td key={id}>
-                  {flexRender(column.columnDef.cell, getContext())}
-                </Td>
-              ))}
-            </Tr>
-          ))}
-        </Tbody>
-      </ChakraTable>
-    </TableContainer>
+    <>
+      <TableContainer>
+        <ChakraTable variant="double-row">
+          <Thead>
+            {table.getHeaderGroups().map(({ id, headers }) => (
+              <Tr key={id}>
+                {headers.map((header) => (
+                  <Th
+                    key={header.id}
+                    onClick={header.column.getToggleSortingHandler()}
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+                  </Th>
+                ))}
+              </Tr>
+            ))}
+          </Thead>
+          <Tbody>
+            {table.getRowModel().rows.map((row) => (
+              <Tr key={row.id}>
+                {row.getVisibleCells().map(({ id, column, getContext }) => (
+                  <Td key={id}>
+                    {flexRender(column.columnDef.cell, getContext())}
+                  </Td>
+                ))}
+              </Tr>
+            ))}
+          </Tbody>
+        </ChakraTable>
+      </TableContainer>
+      <HStack mt={6}>
+        {PAGINATION_BUTTONS.map(({ ariaLabel, onClick, isDisabled, icon }) => (
+          <IconButton
+            variant="ghost"
+            background="white"
+            aria-label={ariaLabel}
+            onClick={() => onClick(table)}
+            isDisabled={isDisabled(table)}
+            icon={icon}
+          />
+        ))}
+        <TextSm>
+          {`Page ${
+            table.getState().pagination.pageIndex + 1
+          } of ${table.getPageCount()}`}
+        </TextSm>
+      </HStack>
+    </>
   )
 }
 
--- dapp/src/hooks/useTransactionHistoryTable.ts
+++ dapp/src/hooks/useTransactionHistoryTable.ts
@@ -1,12 +1,20 @@
 import {
   ColumnDef,
+  PaginationState,
   Table,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table"
 import { StakeHistory } from "#/types"
+import { useState } from "react"
 
-type UseTransactionHistoryTableResult = Table<StakeHistory>
+const PAGINATION_STATE = {
+  pageIndex: 0,
+  pageSize: 2,
+}
+
+export type UseTransactionHistoryTableResult = Table<StakeHistory>
 
 export function useTransactionHistoryTable({
   data,
@@ -15,10 +23,18 @@ export function useTransactionHistoryTable({
   data: StakeHistory[]
   columns: ColumnDef<StakeHistory>[]
 }): UseTransactionHistoryTableResult {
+  const [pagination, setPagination] =
+    useState<PaginationState>(PAGINATION_STATE)
+
   const table = useReactTable({
     columns,
     data,
+    onPaginationChange: setPagination,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    state: {
+      pagination,
+    },
   })
 
   return table
