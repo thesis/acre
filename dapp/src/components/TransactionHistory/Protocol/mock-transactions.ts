c04379d33a8557781fa361179c9a466e9de7cfdd
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-01-15 09:30:05 +0100

    Add mock data for transaction table tests

--- dapp/src/components/TransactionHistory/Protocol/mock-transactions.ts
+++ dapp/src/components/TransactionHistory/Protocol/mock-transactions.ts
@@ -45,4 +45,26 @@ export const TRANSACTIONS: StakeHistory[] = [
       status: 0,
     },
   },
+  {
+    callTx: {
+      timestamp: 1700608973,
+      action: "unstake",
+      asset: "stBTC",
+      amount: 32.1,
+      account: "2MsjRekULh27YSM17p8gSNkVvbXw6wc4kcZ",
+      txHash:
+        "925c8910775c1842fbcfee782104d0d9934dde6f0ca00d393858fcbe8ac90eb7",
+      status: 0,
+    },
+    receiptTx: {
+      timestamp: 1700608973,
+      action: "receive",
+      asset: "BTC",
+      amount: 32.1,
+      account: "0x208e94d5661a73360d9387d3ca169e5c130090cd",
+      txHash:
+        "0xf612b8999e765f9631c5e32a9f424a097936da1c527953e78dc8da20f65bc3ee",
+      status: 0,
+    },
+  },
 ]
