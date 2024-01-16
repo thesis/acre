ae7e61bcb83c1d40bedf141e41f353677b68f5cd
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-01-16 15:31:11 +0100

    Update mock data for tests

--- dapp/src/components/TransactionHistory/Account/mock-transactions.ts
+++ dapp/src/components/TransactionHistory/Account/mock-transactions.ts
@@ -4,9 +4,13 @@ export const TRANSACTIONS: StakeHistory[] = [
   {
     callTx: {
       timestamp: 1700579973,
+      chain: "bitcoin",
       action: "stake",
-      asset: "BTC",
-      amount: 3.11,
+      asset: {
+        currency: "bitcoin",
+        symbol: "BTC",
+        amount: 311000000,
+      },
       account: "2MsjRekULh27YSM17p8gSNkVvbXw6wc4kcZ",
       txHash:
         "925c8910775c1842fbcfee782104d0d9934dde6f0ca00d393858fcbe8ac90eb7",
@@ -14,9 +18,13 @@ export const TRANSACTIONS: StakeHistory[] = [
     },
     receiptTx: {
       timestamp: 1700579973,
+      chain: "ethereum",
       action: "receive",
-      asset: "stBTC",
-      amount: 3.11,
+      asset: {
+        currency: "stbtc",
+        symbol: "stBTC",
+        amount: 311000000,
+      },
       account: "0x208e94d5661a73360d9387d3ca169e5c130090cd",
       txHash:
         "0xf612b8999e765f9631c5e32a9f424a097936da1c527953e78dc8da20f65bc3ee",
--- dapp/src/components/TransactionHistory/Protocol/mock-transactions.ts
+++ dapp/src/components/TransactionHistory/Protocol/mock-transactions.ts
@@ -4,9 +4,13 @@ export const TRANSACTIONS: StakeHistory[] = [
   {
     callTx: {
       timestamp: 1700179973,
+      chain: "bitcoin",
       action: "stake",
-      asset: "BTC",
-      amount: 2.24,
+      asset: {
+        currency: "bitcoin",
+        symbol: "BTC",
+        amount: 224440000,
+      },
       account: "2MsjRekULh27YSM17p8gSNkVvbXw6wc4kcZ",
       txHash:
         "925c8910775c1842fbcfee782104d0d9934dde6f0ca00d393858fcbe8ac90eb7",
@@ -14,9 +18,13 @@ export const TRANSACTIONS: StakeHistory[] = [
     },
     receiptTx: {
       timestamp: 1700179973,
+      chain: "ethereum",
       action: "receive",
-      asset: "stBTC",
-      amount: 2.24,
+      asset: {
+        currency: "stbtc",
+        symbol: "stBTC",
+        amount: 224000000,
+      },
       account: "0x208e94d5661a73360d9387d3ca169e5c130090cd",
       txHash:
         "0xf612b8999e765f9631c5e32a9f424a097936da1c527953e78dc8da20f65bc3ee",
@@ -26,9 +34,13 @@ export const TRANSACTIONS: StakeHistory[] = [
   {
     callTx: {
       timestamp: 1700578973,
+      chain: "bitcoin",
       action: "unstake",
-      asset: "stBTC",
-      amount: 6.14,
+      asset: {
+        currency: "stbtc",
+        symbol: "stBTC",
+        amount: 614000000,
+      },
       account: "2MsjRekULh27YSM17p8gSNkVvbXw6wc4kcZ",
       txHash:
         "925c8910775c1842fbcfee782104d0d9934dde6f0ca00d393858fcbe8ac90eb7",
@@ -36,9 +48,13 @@ export const TRANSACTIONS: StakeHistory[] = [
     },
     receiptTx: {
       timestamp: 1700578973,
+      chain: "ethereum",
       action: "receive",
-      asset: "BTC",
-      amount: 6.14,
+      asset: {
+        currency: "bitcoin",
+        symbol: "BTC",
+        amount: 614000000,
+      },
       account: "0x208e94d5661a73360d9387d3ca169e5c130090cd",
       txHash:
         "0xf612b8999e765f9631c5e32a9f424a097936da1c527953e78dc8da20f65bc3ee",
@@ -48,9 +64,13 @@ export const TRANSACTIONS: StakeHistory[] = [
   {
     callTx: {
       timestamp: 1700608973,
+      chain: "bitcoin",
       action: "unstake",
-      asset: "stBTC",
-      amount: 32.1,
+      asset: {
+        currency: "stbtc",
+        symbol: "stBTC",
+        amount: 321000000,
+      },
       account: "2MsjRekULh27YSM17p8gSNkVvbXw6wc4kcZ",
       txHash:
         "925c8910775c1842fbcfee782104d0d9934dde6f0ca00d393858fcbe8ac90eb7",
@@ -58,9 +78,13 @@ export const TRANSACTIONS: StakeHistory[] = [
     },
     receiptTx: {
       timestamp: 1700608973,
+      chain: "ethereum",
       action: "receive",
-      asset: "BTC",
-      amount: 32.1,
+      asset: {
+        currency: "bitcoin",
+        symbol: "BTC",
+        amount: 321000000,
+      },
       account: "0x208e94d5661a73360d9387d3ca169e5c130090cd",
       txHash:
         "0xf612b8999e765f9631c5e32a9f424a097936da1c527953e78dc8da20f65bc3ee",
