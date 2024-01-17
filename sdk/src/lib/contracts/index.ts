66151052d4ce6895a8cd42baaf30379c32d400df
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-01-17 18:32:45 +0100

    Add docs comments
    Add docs comments to TBTCDepositor interface and to the Ethereum
    implementation of this contract.

--- sdk/src/lib/contracts/index.ts
+++ sdk/src/lib/contracts/index.ts
@@ -3,6 +3,9 @@ import { TBTCDepositor } from "./tbtc-depositor"
 export * from "./tbtc-depositor"
 export * from "./chain-identifier"
 
+/**
+ * Represents all contracts that allow interaction with the Acre network.
+ */
 export type AcreContracts = {
   depositor: TBTCDepositor
 }
