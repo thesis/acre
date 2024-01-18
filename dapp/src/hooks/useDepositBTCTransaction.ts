813abcb5b323e5affaa956dcf876d79bf2572090
ioay <ayioerthi@gmail.com>
2024-01-18 13:19:11 +0100

    Removed comment

--- dapp/src/hooks/useDepositBTCTransaction.ts
+++ dapp/src/hooks/useDepositBTCTransaction.ts
@@ -5,7 +5,6 @@ export function useDepositBTCTransaction(onSuccess?: OnSuccessCallback) {
   // TODO: sending transactions using the SDK
   const depositBTC = useCallback(() => {
     if (onSuccess) {
-      // FIXME: enable eslint rule when SDK ready
       setTimeout(onSuccess, 1000)
     }
   }, [onSuccess])
