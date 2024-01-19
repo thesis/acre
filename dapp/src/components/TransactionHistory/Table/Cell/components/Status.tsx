096599d71732267d92d596394b663771b2e55eed
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-01-19 13:09:31 +0100

    When the transaction is pending display `In queue` text in the table

--- dapp/src/components/TransactionHistory/Table/Cell/components/Status.tsx
+++ dapp/src/components/TransactionHistory/Table/Cell/components/Status.tsx
@@ -1,13 +1,14 @@
 import React from "react"
 import { TransactionInfoStatus } from "#/types"
 import StatusInfo from "#/components/shared/StatusInfo"
+import SimpleText from "./SimpleText"
 
 function Status({ status }: { status: TransactionInfoStatus }) {
   if (status === "syncing")
     return <StatusInfo status={status} withDefaultColor />
 
   if (status === "pending")
-    return <StatusInfo status={status} color="grey.400" />
+    return <SimpleText color="grey.400">In queue</SimpleText>
 
   return <StatusInfo status={status} />
 }
