aa6aad9a24cdf03e9f0ef4b63baaafeba2d7bf44
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-01-23 09:20:33 +0100

    Simplify the logic for creating test transactions

--- dapp/src/components/TransactionHistory/Account/mock-transactions.ts
+++ dapp/src/components/TransactionHistory/Account/mock-transactions.ts
@@ -1,32 +1,20 @@
+import { createTransactionInfo } from "#/tests/factories"
 import { StakeHistory } from "#/types"
 
 export const TRANSACTIONS: StakeHistory[] = [
   {
-    callTx: {
-      timestamp: 1700579973,
-      chain: "bitcoin",
-      action: "stake",
-      asset: {
-        currency: "bitcoin",
-        amount: 311000000,
-      },
-      account: "2MsjRekULh27YSM17p8gSNkVvbXw6wc4kcZ",
-      txHash:
-        "925c8910775c1842fbcfee782104d0d9934dde6f0ca00d393858fcbe8ac90eb7",
-      status: "completed",
-    },
-    receiptTx: {
-      timestamp: 1700579973,
-      chain: "ethereum",
+    callTx: createTransactionInfo({
+      status: "syncing",
+      amount: 224000000,
+    }),
+    receiptTx: createTransactionInfo({
+      amount: 224000000,
       action: "receive",
-      asset: {
-        currency: "stbtc",
-        amount: 311000000,
-      },
+      currency: "bitcoin",
+      chain: "ethereum",
       account: "0x208e94d5661a73360d9387d3ca169e5c130090cd",
-      txHash:
-        "0xf612b8999e765f9631c5e32a9f424a097936da1c527953e78dc8da20f65bc3ee",
-      status: "completed",
-    },
+      txHash: undefined,
+      status: undefined,
+    }),
   },
 ]
--- dapp/src/components/TransactionHistory/Protocol/mock-transactions.ts
+++ dapp/src/components/TransactionHistory/Protocol/mock-transactions.ts
@@ -1,85 +1,18 @@
+import { createTransactionInfo } from "#/tests/factories"
 import { StakeHistory } from "#/types"
 
-export const TRANSACTIONS: StakeHistory[] = [
-  {
-    callTx: {
-      timestamp: 1705918304,
-      chain: "bitcoin",
-      action: "stake",
-      asset: {
-        currency: "bitcoin",
-        amount: 224440000,
-      },
-      account: "2MsjRekULh27YSM17p8gSNkVvbXw6wc4kcZ",
-      txHash:
-        "925c8910775c1842fbcfee782104d0d9934dde6f0ca00d393858fcbe8ac90eb7",
-      status: "completed",
-    },
-    receiptTx: {
-      timestamp: 1700179973,
-      chain: "ethereum",
+export const TRANSACTIONS: StakeHistory[] = Array.from({ length: 22 }, () => {
+  const callTx = createTransactionInfo()
+  return {
+    callTx,
+    receiptTx: createTransactionInfo({
+      amount: callTx.amount,
       action: "receive",
-      asset: {
-        currency: "stbtc",
-        amount: 224000000,
-      },
+      chain: "ethereum",
       account: "0x208e94d5661a73360d9387d3ca169e5c130090cd",
       txHash:
         "0xf612b8999e765f9631c5e32a9f424a097936da1c527953e78dc8da20f65bc3ee",
-      status: "completed",
-    },
-  },
-  {
-    callTx: {
-      timestamp: 1700578973,
-      chain: "bitcoin",
-      action: "unstake",
-      asset: {
-        currency: "stbtc",
-        amount: 614000000,
-      },
-      account: "2MsjRekULh27YSM17p8gSNkVvbXw6wc4kcZ",
-      txHash:
-        "925c8910775c1842fbcfee782104d0d9934dde6f0ca00d393858fcbe8ac90eb7",
-      status: "syncing",
-    },
-    receiptTx: {
-      timestamp: 1700578973,
-      chain: "ethereum",
-      action: "receive",
-      asset: {
-        currency: "bitcoin",
-        amount: 614000000,
-      },
-      account: "0x208e94d5661a73360d9387d3ca169e5c130090cd",
-    },
-  },
-  {
-    callTx: {
-      timestamp: 1700608973,
-      chain: "bitcoin",
-      action: "unstake",
-      asset: {
-        currency: "stbtc",
-        amount: 321000000,
-      },
-      account: "2MsjRekULh27YSM17p8gSNkVvbXw6wc4kcZ",
-      txHash:
-        "925c8910775c1842fbcfee782104d0d9934dde6f0ca00d393858fcbe8ac90eb7",
-      status: "syncing",
-    },
-    receiptTx: {
-      timestamp: 1700608973,
-      chain: "ethereum",
-      action: "receive",
-      asset: {
-        currency: "bitcoin",
-        amount: 321000000,
-      },
-      account: "0x208e94d5661a73360d9387d3ca169e5c130090cd",
-      txHash:
-        "0xf612b8999e765f9631c5e32a9f424a097936da1c527953e78dc8da20f65bc3ee",
-      status: "syncing",
-    },
-  },
-]
+      currency: "bitcoin",
+    }),
+  }
+})
