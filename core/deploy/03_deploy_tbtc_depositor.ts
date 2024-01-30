0c696c7f520e26b2b50cfbfd8afa016f4a3582ef
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-01-30 17:58:10 +0100

    Deploy Acre contracts on Sepolia
    And push deployed artifacts to the repo so we can use it in SDK.

--- core/deploy/03_deploy_tbtc_depositor.ts
+++ core/deploy/03_deploy_tbtc_depositor.ts
@@ -1,4 +1,3 @@
-import { ethers } from "hardhat"
 import type { DeployFunction } from "hardhat-deploy/types"
 import type { HardhatRuntimeEnvironment } from "hardhat/types"
 
@@ -6,16 +5,14 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { getNamedAccounts, deployments } = hre
   const { deployer } = await getNamedAccounts()
 
-  // Bridge goerli address.
-  const bridge = "0x0Cad3257C4B7ec6de1f6926Fbf5714255a6632c3"
-  // TBTC Vault goerli address.
-  const tbtcVault = "0x65eB0562FCe858f8328858c76E689aBedB78621F"
-  // Random address. Acre contract doesn't exist on Goerli network.
-  const acre = ethers.Wallet.createRandom().address
+  const bridge = await deployments.get("Bridge")
+  const tbtcVault = await deployments.get("TBTCVault")
+  const tbtc = await deployments.get("TBTC")
+  const acre = await deployments.get("Acre")
 
-  await deployments.deploy("TbtcDepositorStub", {
+  await deployments.deploy("TbtcDepositor", {
     from: deployer,
-    args: [bridge, tbtcVault, acre],
+    args: [bridge.address, tbtcVault.address, tbtc.address, acre.address],
     log: true,
     waitConfirmations: 1,
   })
@@ -27,4 +24,4 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 export default func
 
 func.tags = ["TbtcDepositor"]
-// func.dependencies = ["TBTC", "Acre"]
+func.dependencies = ["TBTC", "Acre"]
