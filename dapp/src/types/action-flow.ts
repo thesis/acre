c94c23b3f1904656298d2747283de7dca26d959b
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-01-30 16:31:35 +0100

    Move types for action flow to a separate file

---
+++ dapp/src/types/action-flow.ts
@@ -0,0 +1,9 @@
+export type ActionFlowType = "stake" | "unstake"
+
+export const ACTION_FLOW_STEPS_TYPES: Record<
+  ActionFlowType,
+  Record<string, number>
+> = {
+  stake: { OVERVIEW: 1, SIGN_MESSAGE: 2, DEPOSIT_BTC: 3 },
+  unstake: { SIGN_MESSAGE: 1 },
+}
--- dapp/src/types/staking.ts
+++ dapp/src/types/staking.ts
@@ -1,15 +1,5 @@
 import { CurrencyType } from "./currency"
 
-export type ActionFlowType = "stake" | "unstake"
-
-export const ACTION_FLOW_STEPS_TYPES: Record<
-  ActionFlowType,
-  Record<string, number>
-> = {
-  stake: { OVERVIEW: 1, SIGN_MESSAGE: 2, DEPOSIT_BTC: 3 },
-  unstake: { SIGN_MESSAGE: 1 },
-}
-
 export type TokenAmount = {
   amount: bigint
   currency: CurrencyType
