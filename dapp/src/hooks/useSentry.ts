0e7b49d68df739d06c44416a03a9306c28791857
ioay <ayioerthi@gmail.com>
2024-01-31 10:19:45 +0100

    TypeScript IntelliSense for user-defined env variables

--- dapp/src/hooks/useSentry.ts
+++ dapp/src/hooks/useSentry.ts
@@ -6,7 +6,7 @@ export const useSentry = () => {
     const { VITE_SENTRY_SUPPORT, VITE_SENTRY_DSN } = import.meta.env
 
     if (VITE_SENTRY_SUPPORT) {
-      initializeSentry(`${VITE_SENTRY_DSN}`)
+      initializeSentry(VITE_SENTRY_DSN)
     }
   }, [])
 }
