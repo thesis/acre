0a7e8f119fdcf25a483083d5d372b11581e71d60
ioay <ayioerthi@gmail.com>
2024-01-31 17:47:56 +0100

    Added depositReceipt to export utils

--- sdk/src/lib/utils/hex.ts
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export { Hex } from "@keep-network/tbtc-v2.ts"
---
+++ sdk/src/lib/utils/tbtc-v2.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export { Hex, DepositReceipt } from "@keep-network/tbtc-v2.ts"
