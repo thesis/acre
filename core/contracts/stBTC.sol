26af1373011fe30b90c53e1cd451be2c3e68a947
Dmitry <dymitr.paremski@thesis.co>
2024-02-01 13:05:15 +0100

    Moving ERC4626Fess to lib folder
    We are anticipating more third party abstract classes to be added to
    acre. This contract can set the pattern so every other abstract third
    party code can land here.

--- core/contracts/stBTC.sol
+++ core/contracts/stBTC.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Dispatcher.sol";
-import "./ERC4626Fees.sol";
+import "./lib/ERC4626Fees.sol";
 
 /// @title stBTC
 /// @notice This contract implements the ERC-4626 tokenized vault standard. By
