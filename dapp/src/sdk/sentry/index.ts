6997672cd613d321b0ed742d089031102c1aa54c
ioay <ayioerthi@gmail.com>
2024-02-06 20:25:44 +0100

    Default config for BrowserTracing

--- dapp/src/sdk/sentry/index.ts
+++ dapp/src/sdk/sentry/index.ts
@@ -3,12 +3,7 @@ import * as Sentry from "@sentry/react"
 export const initializeSentry = (dsn: string) => {
   Sentry.init({
     dsn,
-    integrations: [
-      new Sentry.BrowserTracing({
-        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: ["localhost"],
-      }),
-    ],
+    integrations: [new Sentry.BrowserTracing()],
     // Performance Monitoring
     // Capture 100% of the transactions
     tracesSampleRate: 1.0,
