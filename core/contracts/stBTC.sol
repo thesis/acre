4c7259130b7d780d4d37385aaf47924664c42173
Jakub Nowakowski <jakub.nowakowski@thesis.co>
2024-02-07 17:09:23 +0100

    Use Ownable2Step in all contracts
    We switch to Ownable2Step as it gives better security against unintended
    onwership transfer.

--- core/contracts/stBTC.sol
+++ core/contracts/stBTC.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "./Dispatcher.sol";
 import "./lib/ERC4626Fees.sol";
 
@@ -18,7 +18,7 @@ import "./lib/ERC4626Fees.sol";
 ///      of yield-bearing vaults. This contract facilitates the minting and
 ///      burning of shares (stBTC), which are represented as standard ERC20
 ///      tokens, providing a seamless exchange with tBTC tokens.
-contract stBTC is ERC4626Fees, Ownable {
+contract stBTC is ERC4626Fees, Ownable2Step {
     using SafeERC20 for IERC20;
 
     /// Dispatcher contract that routes tBTC from stBTC to a given vault and back.
