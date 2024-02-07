b8bb2d35fd3d1719555ecc204c54b5f2c1f5a04b
ioay <ayioerthi@gmail.com>
2024-02-07 15:59:18 +0100

    Added comment to tracesSampleRate prop

--- dapp/src/sdk/sentry/index.ts
+++ dapp/src/sdk/sentry/index.ts
@@ -5,7 +5,7 @@ export const initializeSentry = (dsn: string) => {
     dsn,
     integrations: [new Sentry.BrowserTracing()],
     // Performance Monitoring
-    // Capture 100% of the transactions
+    // TODO: Now, it's capturing 100% of the transactions. It should be changed when prod mode is enabled
     tracesSampleRate: 1.0,
   })
 }
