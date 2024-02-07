a0db19639a53e7176e85ed7bdc4fa68a83dcf554
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-02-07 11:40:21 +0100

    Create a env variable `VITE_ETH_HOSTNAME_HTTP`
    We should setup `JsonRpcProvider` using an environment variable. This
    makes it easy to change addresses.

---
+++ dapp/src/web3/utils/ledger-live-app.ts
@@ -0,0 +1,57 @@
+import {
+  EthereumTransaction,
+  WindowMessageTransport,
+} from "@ledgerhq/wallet-api-client"
+import { TransactionRequest, ZeroAddress, JsonRpcProvider } from "ethers"
+import { Hex } from "@acre/sdk"
+
+export const getLedgerLiveAppWalletAPITransport = () =>
+  new WindowMessageTransport()
+
+export const getLedgerLiveAppProvider = () =>
+  new JsonRpcProvider(import.meta.env.VITE_ETH_HOSTNAME_HTTP)
+
+// Created based on the
+// https://github.com/keep-network/tbtc-v2/blob/main/typescript/src/lib/utils/ledger.ts.
+export function serializeLedgerWalletApiEthereumTransaction(
+  transaction: TransactionRequest,
+): EthereumTransaction {
+  const {
+    value,
+    to,
+    nonce,
+    data,
+    gasPrice,
+    gasLimit,
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+  } = transaction
+
+  const ethereumTransaction: EthereumTransaction = {
+    family: "ethereum" as const,
+    // @ts-expect-error We do not want to install external bignumber.js lib so
+    // here we use bigint. The Ledger Wallet Api just converts the bignumber.js
+    // object to string so we can pass bigint. See:
+    // https://github.com/LedgerHQ/wallet-api/blob/main/packages/core/src/families/ethereum/serializer.ts#L4
+    amount: value ?? 0,
+    recipient: to?.toString() || ZeroAddress,
+    nonce: nonce ?? undefined,
+    // @ts-expect-error See comment above.
+    gasPrice: gasPrice ?? undefined,
+    // @ts-expect-error See comment above.
+    gasLimit: gasLimit ?? undefined,
+    // @ts-expect-error See comment above.
+    maxFeePerGas: maxFeePerGas ?? undefined,
+    // @ts-expect-error See comment above.
+    maxPriorityFeePerGas: maxPriorityFeePerGas ?? undefined,
+  }
+
+  if (nonce) ethereumTransaction.nonce = Number(nonce)
+  if (data)
+    ethereumTransaction.data = Buffer.from(
+      Hex.from(data.toString()).toString(),
+      "hex",
+    )
+
+  return ethereumTransaction
+}
