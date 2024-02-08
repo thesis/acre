6b637f039b584103276065f863b819b2deba4f53
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-02-08 14:17:58 +0100

    Remove unnecessary code

--- dapp/src/components/Overview/index.tsx
+++ dapp/src/components/Overview/index.tsx
@@ -1,27 +1,14 @@
-import React, { useCallback } from "react"
-import { Button, Flex, Grid, HStack, Icon, Switch } from "@chakra-ui/react"
-import { useDocsDrawer, useWalletContext } from "#/hooks"
+import React from "react"
+import { Flex, Grid, HStack, Switch } from "@chakra-ui/react"
+import { useDocsDrawer } from "#/hooks"
 import { TextSm } from "#/components/shared/Typography"
 import { USD } from "#/constants"
-import { useStakeFlow } from "#/acre-react/hooks"
 import PositionDetails from "./PositionDetails"
 import Statistics from "./Statistics"
 import TransactionHistory from "./TransactionHistory"
-import { useAcreContext } from "../../acre-react/AcreSdkContext"
 
 export default function Overview() {
   const { onOpen } = useDocsDrawer()
-  const { ethAccount } = useWalletContext()
-  const ethAddress = ethAccount?.address
-
-  const { init, isInitialized } = useAcreContext()
-  const { initStake, btcAddress, signMessage, stake } = useStakeFlow()
-
-  const initSDK = useCallback(async () => {
-    if (!ethAddress) return
-
-    await init(ethAddress, "sepolia")
-  }, [ethAddress, init])
 
   return (
     <Flex direction="column" gap={2} p={6}>
@@ -30,37 +17,6 @@ export default function Overview() {
           {/* TODO: Handle click actions */}
           <Switch size="sm" />
           <TextSm fontWeight="bold">Show values in {USD.symbol}</TextSm>
-          <Button
-            isDisabled={isInitialized || !ethAddress}
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            onClick={initSDK}
-          >
-            Connect sdk
-          </Button>
-          <Button
-            isDisabled={!isInitialized}
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            onClick={async () => {
-              await initStake("mjc2zGWypwpNyDi4ZxGbBNnUA84bfgiwYc", ethAddress!)
-            }}
-          >
-            Initialize stake
-          </Button>
-          <Button
-            isDisabled={!btcAddress}
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            onClick={signMessage}
-          >
-            Sign message
-          </Button>
-          <TextSm>Deposit address: {btcAddress ?? "not available yet"}</TextSm>
-          <Button
-            isDisabled={!btcAddress}
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            onClick={stake}
-          >
-            Stake
-          </Button>
         </HStack>
         <ButtonLink colorScheme="gold" bg="gold.200" onClick={onOpen}>
           Docs
