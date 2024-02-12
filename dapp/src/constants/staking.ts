f5dae13139a026f521d14606ce290dc9db7a4ef9
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-02-12 08:27:08 +0100

    Update a minimum amount of BTC deposit
    The minimum deposit value in the tBTC Bridge on Sepolia was set to 100000 (0.001 BTC). Let's change it here for tests. However, let's change this value later to the expected value from the SDK.

--- dapp/src/constants/staking.ts
+++ dapp/src/constants/staking.ts
@@ -1,3 +1,4 @@
-export const BITCOIN_MIN_AMOUNT = "1000000" // 0.01 BTC
+// TODO: Read the value from the SDK, once we expose it
+export const BITCOIN_MIN_AMOUNT = "100000" // 0.001 BTC
 
 export const REFERRAL = import.meta.env.VITE_REFERRAL
