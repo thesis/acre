04400d5bef7f9d6d44288af3bfacfc365372d91d
ioay <ayioerthi@gmail.com>
2024-02-13 16:25:55 +0100

    Added mocked PollStats & recharts package

---
+++ dapp/src/components/PoolStats/mocked-stats.ts
@@ -0,0 +1,57 @@
+export const mockedStats = [
+  {
+    year: "2023",
+    month: "Jan",
+    value: "3",
+  },
+  {
+    year: "2023",
+    month: "Feb",
+    value: "4",
+  },
+  {
+    year: "2023",
+    month: "Mar",
+    value: "6",
+  },
+  {
+    year: "2023",
+    month: "Apr",
+    value: "3",
+  },
+  {
+    year: "2023",
+    month: "May",
+    value: "4",
+  },
+  {
+    year: "2023",
+    month: "Jun",
+    value: "3",
+  },
+  {
+    year: "2023",
+    month: "Jul",
+    value: "5",
+  },
+  {
+    year: "2023",
+    month: "Aug",
+    value: "3",
+  },
+  {
+    year: "2023",
+    month: "Sep",
+    value: "2",
+  },
+  {
+    year: "2023",
+    month: "Oct",
+    value: "3",
+  },
+  {
+    year: "2023",
+    month: "Dec",
+    value: "5",
+  },
+]
