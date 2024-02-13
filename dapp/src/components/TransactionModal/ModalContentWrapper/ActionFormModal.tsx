e269c3a5a46c0aaf23fa2ff8113f76e01bbae3f4
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-02-13 15:05:46 +0100

    Create a `FormSubmitButton` component

--- dapp/src/components/TransactionModal/ModalContentWrapper/ActionFormModal.tsx
+++ dapp/src/components/TransactionModal/ModalContentWrapper/ActionFormModal.tsx
@@ -86,10 +86,7 @@ function ActionFormModal({ defaultType }: { defaultType: ActionFlowType }) {
           </TabList>
           <TabPanels>
             <TabPanel>
-              <StakeFormModal
-                onSubmitForm={handleSubmitFormWrapper}
-                isLoading={isLoading}
-              />
+              <StakeFormModal onSubmitForm={handleSubmitFormWrapper} />
             </TabPanel>
             <TabPanel>
               {/* TODO: Use the correct form for unstaking */}
---
+++ dapp/src/components/shared/Form/FormSubmitButton.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { useFormikContext } from "formik"
+import { Button, ButtonProps } from "@chakra-ui/react"
+import Spinner from "../Spinner"
+
+const LOADING_STYLE = {
+  _disabled: { background: "gold.300", opacity: 1 },
+  _hover: { opacity: 1 },
+}
+
+export function FormSubmitButton({ ...props }: ButtonProps) {
+  const { isSubmitting } = useFormikContext()
+
+  return (
+    <Button
+      type="submit"
+      size="lg"
+      width="100%"
+      mt={4}
+      isLoading={isSubmitting}
+      spinner={<Spinner />}
+      {...(isSubmitting && LOADING_STYLE)}
+      {...props}
+    >
+      Stake
+    </Button>
+  )
+}
