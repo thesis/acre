3501a3143b0ee1b4d2795e469ef3793b3b14d21f
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-02-13 10:14:09 +0100

    Fix incorrect value for bitcoin min amount

--- dapp/src/constants/staking.ts
+++ dapp/src/constants/staking.ts
@@ -1,7 +1,7 @@
 import { getDesiredDecimals } from "#/utils/numbers"
 
 // TODO: Read the value from the SDK, once we expose it
-export const BITCOIN_MIN_AMOUNT = String(1e5) // 0.0001 BTC
+export const BITCOIN_MIN_AMOUNT = String(1e4) // 0.0001 BTC
 
 export const BITCOIN_DESIRED_DECIMALS = getDesiredDecimals(
   BITCOIN_MIN_AMOUNT,
