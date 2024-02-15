39f6bc7484134e11550495977539f69828e36571
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-02-15 12:37:17 +0100

    Add docs comments to `RelayerDepositorProxy`

--- dapp/src/web3/relayer-depositor-proxy.ts
+++ dapp/src/web3/relayer-depositor-proxy.ts
@@ -12,12 +12,31 @@ import axios from "axios"
 
 const DEFENDER_WEBHOOK_URL = import.meta.env.VITE_DEFENDER_RELAYER_WEBHOOK_URL
 
+/**
+ * Implementation of @see DepositorProxy that redirects stake requests to the
+ * Open Zeppelin Defender Relayer which initializes stake on the user's behalf.
+ * Sends the HTTP POST request to the webhook at the provided URL with the data
+ * necessary to initialize stake.
+ */
 class RelayerDepositorProxy<T extends TBTCDepositor> implements DepositorProxy {
+  /**
+   * Chain-specific handle to @see TBTCDepositor contract.
+   */
   #tbtcDepositor: T
 
+  /**
+   * Defines the Open Zeppelin Defender webhook URL.
+   */
   #defenderWebhookUrl: string
 
-  static fromEthereumTbtcDepositor(tbtcDepositor: EthereumTBTCDepositor) {
+  /**
+   * Creates the instance of the relayer depositor proxy for Ethereum chain.
+   * @param tbtcDepositor Ethereum handle to @see TBTCDepositor contract.
+   * @returns Instance of @see RelayerDepositorProxy.
+   */
+  static fromEthereumTbtcDepositor(
+    tbtcDepositor: EthereumTBTCDepositor,
+  ): RelayerDepositorProxy<EthereumTBTCDepositor> {
     return new RelayerDepositorProxy(tbtcDepositor, DEFENDER_WEBHOOK_URL)
   }
 
@@ -26,10 +45,17 @@ class RelayerDepositorProxy<T extends TBTCDepositor> implements DepositorProxy {
     this.#defenderWebhookUrl = _defenderWebhookUr
   }
 
+  /**
+   * @see {DepositorProxy#getChainIdentifier}
+   */
   getChainIdentifier(): ChainIdentifier {
     return this.#tbtcDepositor.getChainIdentifier()
   }
 
+  /**
+   * @see {DepositorProxy#revealDeposit}
+   * @dev Sends HTTP POST request to Open Zeppelin Defender Relayer.
+   */
   async revealDeposit(
     depositTx: BitcoinRawTxVectors,
     depositOutputIndex: number,
