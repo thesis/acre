161ea5559e13aef162aa245feac325e02b5c5782
Jakub Nowakowski <jakub.nowakowski@thesis.co>
2024-02-16 13:17:20 +0100

    Add missing Math import

--- core/contracts/AcreBitcoinDepositor.sol
+++ core/contracts/AcreBitcoinDepositor.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.21;
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 import "@keep-network/tbtc-v2/contracts/integrator/AbstractTBTCDepositor.sol";
 
