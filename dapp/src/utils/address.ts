af42a80cb4c7a5a18fbd1a6239b8a526571f15d0
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-02-26 14:03:56 +0100

    Add a doc  for `isSupportedBTCAddressType`

--- dapp/src/utils/address.ts
+++ dapp/src/utils/address.ts
@@ -5,5 +5,6 @@ export function truncateAddress(address: string): string {
   return `${address.slice(0, 6)}…${address.slice(-5)}`
 }
 
+// tBTC v2 deposit process supports only 2PKH or P2WPKH Bitcoin address
 export const isSupportedBTCAddressType = (address: string): boolean =>
   isPublicKeyHashTypeAddress(address, BITCOIN_NETWORK)
