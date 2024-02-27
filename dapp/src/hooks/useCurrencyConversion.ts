9861fe6280cd0ee4277365472ea0d2b30e1db2e4
ioay <ayioerthi@gmail.com>
2024-02-27 16:20:54 +0100

    Added memoization for useCurrencyConversion hook

--- dapp/src/hooks/useCurrencyConversion.ts
+++ dapp/src/hooks/useCurrencyConversion.ts
@@ -2,6 +2,7 @@ import { CURRENCIES_BY_TYPE } from "#/constants"
 import { selectBtcUsdPrice } from "#/store/btc"
 import { bigIntToUserAmount } from "#/utils"
 import { CurrencyConversionType } from "#/types"
+import { useMemo } from "react"
 import { useAppSelector } from "./store"
 
 export function useCurrencyConversion(
@@ -10,21 +11,25 @@ export function useCurrencyConversion(
 ) {
   const usdPrice = useAppSelector(selectBtcUsdPrice)
 
-  if (!from.amount || BigInt(from.amount) < 0n) return undefined
+  const convertedValue = useMemo(() => {
+    if (!from.amount || BigInt(from.amount) < 0n) return undefined
 
-  switch (from.currency) {
-    case "bitcoin":
-      if (to.currency === "usd") {
-        const fromAmount = bigIntToUserAmount(
-          BigInt(from.amount),
-          CURRENCIES_BY_TYPE[from.currency].decimals,
-          CURRENCIES_BY_TYPE[from.currency].desiredDecimals,
-        )
-        const conversionAmount = fromAmount * usdPrice
-        return conversionAmount
-      }
-      return undefined
-    default:
-      return undefined
-  }
+    switch (from.currency) {
+      case "bitcoin":
+        if (to.currency === "usd") {
+          const fromAmount = bigIntToUserAmount(
+            BigInt(from.amount),
+            CURRENCIES_BY_TYPE[from.currency].decimals,
+            CURRENCIES_BY_TYPE[from.currency].desiredDecimals,
+          )
+          const conversionAmount = fromAmount * usdPrice
+          return conversionAmount
+        }
+        return undefined
+      default:
+        return undefined
+    }
+  }, [from, to, usdPrice])
+
+  return convertedValue
 }
