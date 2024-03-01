7062cedaf1c3115b38bb230c577fc31bd2835ad1
ioay <ayioerthi@gmail.com>
2024-03-01 11:59:47 +0100

    Created useFetchBTCPriceUsd hook, utils structure updated

--- dapp/src/hooks/useInitApp.ts
+++ dapp/src/hooks/useInitApp.ts
@@ -1,19 +1,11 @@
-import { useEffect } from "react"
-import { fetchBTCPriceUSD } from "#/store/btc"
-import { logPromiseFailure } from "#/utils"
-import { useAppDispatch } from "./store"
-import { useInitializeAcreSdk } from "./useInitializeAcreSdk"
 import { useSentry } from "./sentry"
+import { useInitializeAcreSdk } from "./useInitializeAcreSdk"
+import { useFetchBTCPriceUSD } from "./useFetchBTCPriceUSD"
 
 export function useInitApp() {
-  const dispatch = useAppDispatch()
-
   // TODO: Let's uncomment when dark mode is ready
   // useDetectThemeMode()
   useSentry()
   useInitializeAcreSdk()
-
-  useEffect(() => {
-    logPromiseFailure(dispatch(fetchBTCPriceUSD()))
-  }, [dispatch])
+  useFetchBTCPriceUSD()
 }
--- dapp/src/utils/exchangeApi/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./coingeckoApi"
-export * from "./verifyDepositAddress"
