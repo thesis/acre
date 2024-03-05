a939ccc50b75705af7a3352788cee932948af0a2
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-03-05 11:59:32 +0100

    Fix typo for SDK docs
    Co-authored-by: Rafał Czajkowski <57687279+r-czajkowski@users.noreply.github.com>

--- sdk/src/lib/bitcoin/address.ts
+++ sdk/src/lib/bitcoin/address.ts
@@ -5,7 +5,7 @@ import {
 } from "@keep-network/tbtc-v2.ts"
 
 /**
- * Checks if the address is of type P2PKH or P2WPKH
+ * Checks if the address is of type P2PKH or P2WPKH.
  * @param address The address to be checked.
  * @param network The network for which the check will be done.
  */
