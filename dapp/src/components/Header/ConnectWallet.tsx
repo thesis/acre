27ac2ae82ee302e7ce5e68e0ee9f6636636d0ef3
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-03-08 10:35:49 +0100

    Add a tooltip for not supported Bitcoin  address

--- dapp/src/components/Header/ConnectWallet.tsx
+++ dapp/src/components/Header/ConnectWallet.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { HStack } from "@chakra-ui/react"
+import { Button, HStack, Icon, Tooltip } from "@chakra-ui/react"
 import {
   useRequestBitcoinAccount,
   useRequestEthereumAccount,
@@ -8,12 +8,42 @@ import {
 import { CurrencyBalance } from "#/components/shared/CurrencyBalance"
 import { TextMd } from "#/components/shared/Typography"
 import { Bitcoin, EthereumIcon } from "#/assets/icons"
-import { ConnectButton } from "./ConnectButton"
+import { Account } from "@ledgerhq/wallet-api-client"
+import { CURRENCY_ID_BITCOIN } from "#/constants"
+import {
+  isSupportedBTCAddressType,
+  logPromiseFailure,
+  truncateAddress,
+} from "#/utils"
+
+const getCustomDataByAccount = (
+  account?: Account,
+): { text: string; colorScheme?: string } => {
+  if (!account) return { text: "Not connected", colorScheme: "error" }
+
+  const { address, currency } = account
+
+  if (currency === CURRENCY_ID_BITCOIN && !isSupportedBTCAddressType(address))
+    return { text: "Not supported", colorScheme: "error" }
+
+  return { text: truncateAddress(address) }
+}
 
 export default function ConnectWallet() {
   const { requestAccount: requestBitcoinAccount } = useRequestBitcoinAccount()
   const { requestAccount: requestEthereumAccount } = useRequestEthereumAccount()
   const { btcAccount, ethAccount } = useWalletContext()
+
+  const dataBtcAccount = getCustomDataByAccount(btcAccount)
+  const dataEthAccount = getCustomDataByAccount(ethAccount)
+
+  const handleConnectBitcoinAccount = () => {
+    logPromiseFailure(requestBitcoinAccount())
+  }
+
+  const handleConnectEthereumAccount = () => {
+    logPromiseFailure(requestEthereumAccount())
+  }
 
   return (
     <HStack spacing={4}>
@@ -24,20 +54,30 @@ export default function ConnectWallet() {
           amount={btcAccount?.balance.toString()}
         />
       </HStack>
-      <ConnectButton
-        leftIcon={Bitcoin}
-        account={btcAccount}
-        requestAccount={async () => {
-          await requestBitcoinAccount()
-        }}
-      />
-      <ConnectButton
-        leftIcon={EthereumIcon}
-        account={ethAccount}
-        requestAccount={async () => {
-          await requestEthereumAccount()
-        }}
-      />
+      <Tooltip
+        label="Currently, we support only Legacy or Native SegWit addresses. Please try connecting another address."
+        placement="top"
+        isDisabled={
+          !(btcAccount && !isSupportedBTCAddressType(btcAccount.address))
+        }
+      >
+        <Button
+          variant="card"
+          colorScheme={dataBtcAccount.colorScheme}
+          leftIcon={<Icon as={Bitcoin} boxSize={6} />}
+          onClick={handleConnectBitcoinAccount}
+        >
+          {dataBtcAccount.text}
+        </Button>
+      </Tooltip>
+      <Button
+        variant="card"
+        colorScheme={dataEthAccount.colorScheme}
+        leftIcon={<Icon as={EthereumIcon} boxSize={6} />}
+        onClick={handleConnectEthereumAccount}
+      >
+        {dataEthAccount.text}
+      </Button>
     </HStack>
   )
 }
