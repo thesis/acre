d1d705f57543da39cc66e72e852b4e3c61072b74
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-03-08 14:09:01 +0100

    Make `AcreBitcoinDepositor` contract upgradable

--- core/deploy/03_deploy_acre_bitcoin_depositor.ts
+++ core/deploy/03_deploy_acre_bitcoin_depositor.ts
@@ -1,29 +1,36 @@
 import type { DeployFunction } from "hardhat-deploy/types"
 import type { HardhatRuntimeEnvironment } from "hardhat/types"
-import { waitConfirmationsNumber } from "../helpers/deployment"
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const { getNamedAccounts, deployments, helpers } = hre
-  const { deployer } = await getNamedAccounts()
+  const { deployments, helpers } = hre
+  const { deployer } = await helpers.signers.getNamedSigners()
 
   const bridge = await deployments.get("Bridge")
   const tbtcVault = await deployments.get("TBTCVault")
   const tbtc = await deployments.get("TBTC")
   const stbtc = await deployments.get("stBTC")
 
-  const depositor = await deployments.deploy("AcreBitcoinDepositor", {
-    contract:
-      process.env.HARDHAT_TEST === "true"
-        ? "AcreBitcoinDepositorHarness"
-        : "AcreBitcoinDepositor",
-    from: deployer,
-    args: [bridge.address, tbtcVault.address, tbtc.address, stbtc.address],
-    log: true,
-    waitConfirmations: waitConfirmationsNumber(hre),
-  })
+  const [, proxyDeployment] = await helpers.upgrades.deployProxy(
+    "AcreBitcoinDepositor",
+    {
+      contractName:
+        process.env.HARDHAT_TEST === "true"
+          ? "AcreBitcoinDepositorHarness"
+          : "AcreBitcoinDepositor",
+      factoryOpts: {
+        signer: deployer,
+      },
+      initializerArgs: [
+        bridge.address,
+        tbtcVault.address,
+        tbtc.address,
+        stbtc.address,
+      ],
+    },
+  )
 
   if (hre.network.tags.etherscan) {
-    await helpers.etherscan.verify(depositor)
+    await helpers.etherscan.verify(proxyDeployment)
   }
 
   // TODO: Add Tenderly verification
