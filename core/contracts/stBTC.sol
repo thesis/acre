dada0531e29ee36a9df153becaffad7553f253c6
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-03-11 10:19:40 +0100

    Remove unnecessary import
    We want to use upgradeable version of `ERC4626` contract so this import
    is unnecessary.

--- core/contracts/stBTC.sol
+++ core/contracts/stBTC.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.21;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
