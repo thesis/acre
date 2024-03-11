733c3c83203223a9077b68bb1504a2867d368851
Dmitry <dymitr.paremski@thesis.co>
2024-03-11 21:37:55 +0100

    Removing unused OZ ERC4626 lib

--- core/contracts/stBTC.sol
+++ core/contracts/stBTC.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.21;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Dispatcher.sol";
