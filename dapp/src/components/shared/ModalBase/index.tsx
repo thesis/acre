4bc66813231601713932f53d6634bbfa7b0b3ccf
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-03-12 11:25:57 +0100

    Style improvements for the modal component

--- dapp/src/components/shared/ModalBase/index.tsx
+++ dapp/src/components/shared/ModalBase/index.tsx
@@ -3,7 +3,7 @@ import { Modal, ModalContent, ModalOverlay, ModalProps } from "@chakra-ui/react"
 
 export default function ModalBase({ children, ...restProps }: ModalProps) {
   return (
-    <Modal size="lg" {...restProps}>
+    <Modal scrollBehavior="inside" size="lg" {...restProps}>
       <ModalOverlay mt="header_height" />
       <ModalContent mt="modal_shift">{children}</ModalContent>
     </Modal>
--- dapp/src/theme/utils/semanticTokens.ts
+++ dapp/src/theme/utils/semanticTokens.ts
@@ -1,7 +1,7 @@
 export const semanticTokens = {
   space: {
     header_height: 24,
-    modal_shift: 36,
+    modal_shift: 44,
   },
   sizes: {
     sidebar_width: 80,
