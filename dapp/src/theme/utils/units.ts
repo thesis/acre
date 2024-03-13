f1218a705aa0364c753f29d726d22233fe6e4bd1
ioay <ayioerthi@gmail.com>
2024-03-13 13:23:10 +0100

    Added chakraSpacingUnit

--- dapp/src/theme/utils/units.ts
+++ dapp/src/theme/utils/units.ts
@@ -1,2 +1,7 @@
 // The values are proportional, so 1 spacing unit is equal to 0.25rem, which translates to 4px by default in common browsers.
-export const chakraUnitToPx = (chakraUnit: number): number => chakraUnit * 4
+const chakraSpacingUnit = {
+  px: 4,
+  rem: 0.25,
+}
+export const chakraUnitToPx = (chakraUnit: number): number =>
+  chakraUnit * chakraSpacingUnit.px
