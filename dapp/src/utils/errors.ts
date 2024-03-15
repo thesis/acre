aa8113ef3364034f95916d0b428915317f37c680
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-03-15 09:13:07 +0100

    Update of error texts for dApp

--- dapp/src/utils/errors.ts
+++ dapp/src/utils/errors.ts
@@ -1,5 +1,5 @@
 export const ERRORS = {
-  SIGNING: "Message signing error.",
-  DEPOSIT_TRANSACTION: "Deposit transaction execution error.",
+  SIGNING: "Message signing interrupted.",
+  DEPOSIT_TRANSACTION: "Deposit transaction execution interrupted.",
   WALLET_NOT_CONNECTED: (type: string) => `${type} wallet is not connected.`,
 }
