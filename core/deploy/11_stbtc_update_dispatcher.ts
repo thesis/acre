0476309be59eb26479f7359f87ca9cf0086334bd
Dmitry <dymitr.paremski@thesis.co>
2024-03-21 16:33:06 +0100

    Replacing Dispatcher with MezoAllocator
    At first Mezo Allocator will be Acre's dispatcher. Later on a proper
    dispatcher will be implemented and switched with the Mezo Allocator.

---
+++ core/deploy/11_stbtc_update_dispatcher.ts
@@ -0,0 +1,21 @@
+import type { HardhatRuntimeEnvironment } from "hardhat/types"
+import type { DeployFunction } from "hardhat-deploy/types"
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { getNamedAccounts, deployments } = hre
+  const { deployer } = await getNamedAccounts()
+
+  const dispatcher = await deployments.get("MezoAllocator")
+
+  await deployments.execute(
+    "stBTC",
+    { from: deployer, log: true, waitConfirmations: 1 },
+    "updateDispatcher",
+    dispatcher.address,
+  )
+}
+
+export default func
+
+func.tags = ["stBTCUpdateDispatcher"]
+func.dependencies = ["stBTC", "Dispatcher"]
