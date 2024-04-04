41a49cdedad4ca0362363d06e49bec84a0254d19
Jakub Nowakowski <jakub.nowakowski@thesis.co>
2024-04-04 13:17:27 +0200

    Move deployment script for pasue admin
    Move the pasue admin setter to execute before ownership transfer.

--- core/deploy/14_update_pause_admin_stbtc.ts
+++ core/deploy/14_update_pause_admin_stbtc.ts
@@ -3,14 +3,14 @@ import type { DeployFunction } from "hardhat-deploy/types"
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { getNamedAccounts, deployments } = hre
-  const { governance, pauseAdmin } = await getNamedAccounts()
+  const { deployer, pauseAdmin } = await getNamedAccounts()
   const { log } = deployments
 
   log(`updating pause admin account of stBTC contract to ${pauseAdmin}`)
 
   await deployments.execute(
     "stBTC",
-    { from: governance, log: true, waitConfirmations: 1 },
+    { from: deployer, log: true, waitConfirmations: 1 },
     "updatePauseAdmin",
     pauseAdmin,
   )
