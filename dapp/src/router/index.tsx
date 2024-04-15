5325dbc93610f9d5cfcf99f8eac881705ea8fbb3
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-04-16 00:42:08 +0200

    Implement router transitions
    Now content slides in and out in y-axis as the route changes.
    Also refactored router creation strategy to use components instead of
    factory function.

--- dapp/src/router/index.tsx
+++ dapp/src/router/index.tsx
@@ -1,16 +1,22 @@
 import React from "react"
-import { createBrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import OverviewPage from "#/pages/OverviewPage"
 import ActivityPage from "#/pages/ActivityPage"
+import Layout from "#/components/shared/Layout"
 import { routerPath } from "./path"
 
-export const router = createBrowserRouter([
-  {
-    path: routerPath.home,
-    element: <OverviewPage />,
-  },
-  {
-    path: `${routerPath.activity}/:activityId`,
-    element: <ActivityPage />,
-  },
-])
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index path={routerPath.home} element={<OverviewPage />} />
+          <Route
+            path={`${routerPath.activity}/:activityId`}
+            element={<ActivityPage />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
