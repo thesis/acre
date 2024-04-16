b09220e2977e15e3443e8ec6cd7ab01663eda178
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-04-16 11:43:36 +0200

    Apply changes in router

--- dapp/src/router/index.tsx
+++ dapp/src/router/index.tsx
@@ -1,16 +1,21 @@
 import React from "react"
 import { createBrowserRouter } from "react-router-dom"
-import OverviewPage from "#/pages/OverviewPage"
+import LandingPage from "#/pages/LandingPage"
 import ActivityPage from "#/pages/ActivityPage"
 import { routerPath } from "./path"
 
 export const router = createBrowserRouter([
   {
     path: routerPath.home,
-    element: <OverviewPage />,
+    element: <LandingPage />,
+    index: true,
   },
   {
     path: `${routerPath.activity}/:activityId`,
     element: <ActivityPage />,
   },
+  // {
+  //   path: `${routerPath.activity}/:activityId`,
+  //   element: <ActivityPage />,
+  // },
 ])
