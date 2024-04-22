d81ace3a0e10f2bf84065407d5e6753c9cb7251e
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-04-22 18:38:50 +0200

    Align content to center
    Added responsive font sizes

--- dapp/src/pages/LandingPage/HeroSection.tsx
+++ dapp/src/pages/LandingPage/HeroSection.tsx
@@ -3,11 +3,20 @@ import { Button, Heading, VStack, Text } from "@chakra-ui/react"
 
 export default function HeroSection() {
   return (
-    <VStack spacing={0} mt={13} mb={20}>
-      <Heading fontSize="6xl" mb={2} fontWeight="semibold">
+    <VStack spacing={0} mt={13} mb={20} align="center" textAlign="center">
+      <Heading
+        fontSize={{ base: "4xl", md: "6xl" }}
+        mb={2}
+        fontWeight="semibold"
+      >
         Bitcoin staking done right.
       </Heading>
-      <Text fontSize="xl" lineHeight={7} mb={10} fontWeight="medium">
+      <Text
+        fontSize={{ base: "md", md: "xl" }}
+        lineHeight={7}
+        mb={10}
+        fontWeight="medium"
+      >
         The open source, decentralized way to grow your bitcoin
       </Text>
       <Button size="lg" px={7} fontWeight="bold" lineHeight={6} h="auto">
