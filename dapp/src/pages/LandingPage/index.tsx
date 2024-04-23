eb3cb022719028205e8fd8b8da85a9fefac11a19
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-04-23 19:30:09 +0200

    Resolve Eslint error

--- dapp/src/pages/LandingPage/index.tsx
+++ dapp/src/pages/LandingPage/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box, Flex } from "@chakra-ui/react"
+import { Flex } from "@chakra-ui/react"
 import SeasonCountdownSection from "./SeasonCountdownSection"
 import HeroSection from "./HeroSection"
 
