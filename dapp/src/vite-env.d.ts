ac7685a3cb118136ef273dd649b417047d42be96
Jakub Nowakowski <jakub.nowakowski@thesis.co>
2024-04-30 12:44:50 +0200

    Pass TBTC_API_ENDPOINT to SDK initialization
    SDK expects tBTC API URL to be provided for initialization. We expect it
    ot be defined in the dApp as env variable.

--- dapp/src/vite-env.d.ts
+++ dapp/src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_ETH_HOSTNAME_HTTP: string
   readonly VITE_REFERRAL: number
   readonly VITE_DEFENDER_RELAYER_WEBHOOK_URL: string
+  readonly VITE_TBTC_API_ENDPOINT: string
 }
 
 interface ImportMeta {
