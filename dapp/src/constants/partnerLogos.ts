1b8eab9e305c2c3217e2ee8cc9335d5df231bc19
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-05-03 15:03:19 +0200

    Refactor partner logos export

--- dapp/src/constants/partnerLogos.ts
+++ dapp/src/constants/partnerLogos.ts
@@ -1,12 +1,9 @@
 import { ImageProps } from "@chakra-ui/react"
-import baseLogo from "#/assets/images/partner-logos/base-logo.svg"
-import thresholdLogo from "#/assets/images/partner-logos/threshold-logo.svg"
-import ledgerLogo from "#/assets/images/partner-logos/ledger-logo.svg"
-import wormholeLogo from "#/assets/images/partner-logos/wormhole-logo.svg"
+import * as partnerLogos from "#/assets/images/partner-logos"
 
 export const PARTNER_LOGOS: Pick<ImageProps, "src" | "maxW" | "alt">[] = [
-  { src: baseLogo, maxW: "5.625rem", alt: "Base logo" }, // 90px
-  { src: thresholdLogo, maxW: "13.125rem", alt: "Threshold logo" }, // 210px
-  { src: ledgerLogo, maxW: "7.4375rem", alt: "Ledger logo" }, // 119px
-  { src: wormholeLogo, maxW: "11.375rem", alt: "Wormhole logo" }, // 182px
+  { src: partnerLogos.baseLogo, maxW: "5.625rem", alt: "Base logo" }, // 90px
+  { src: partnerLogos.thresholdLogo, maxW: "13.125rem", alt: "Threshold logo" }, // 210px
+  { src: partnerLogos.ledgerLogo, maxW: "7.4375rem", alt: "Ledger logo" }, // 119px
+  { src: partnerLogos.wormholeLogo, maxW: "11.375rem", alt: "Wormhole logo" }, // 182px
 ]
