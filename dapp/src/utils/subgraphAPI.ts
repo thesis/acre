da1e61c2bd95836b4a4da613a636e740ced058cb
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-05-07 09:02:17 +0200

    Subgraph integration with dApp
    Subgraph can be run locally or use a production URL. DApp can select the right data source environment variables. The available functions for the Acre subgraph were exported in the `subgraphAPI` object. Currently, only the method that fetches activities has been implemented.

---
+++ dapp/src/utils/subgraphAPI.ts
@@ -0,0 +1,70 @@
+import axios from "axios"
+import { Activity, ActivityDataResponse } from "#/types"
+
+const {
+  VITE_USE_LOCALHOST_SUBGRAPH,
+  VITE_SUBGRAPH_LOCALHOST_URL,
+  VITE_SUBGRAPH_ID,
+  VITE_SUBGRAPH_API_KEY,
+} = import.meta.env
+
+const ACRE_SUBGRAPH_URL = (() => {
+  switch (VITE_USE_LOCALHOST_SUBGRAPH) {
+    case "true":
+      return VITE_SUBGRAPH_LOCALHOST_URL
+    case "false":
+      return `https://gateway-testnet-arbitrum.network.thegraph.com/api/${VITE_SUBGRAPH_API_KEY}/subgraphs/id/${VITE_SUBGRAPH_ID}`
+    default:
+      return ""
+  }
+})()
+
+const mapToActivity = (activityData: ActivityDataResponse): Activity => {
+  const { bitcoinTransactionId: txHash, amountToDeposit, events } = activityData
+
+  const status = events.some(({ type }) => type === "Finalized")
+    ? "completed"
+    : "pending"
+
+  return {
+    txHash,
+    amount: BigInt(amountToDeposit),
+    type: "deposit",
+    status,
+  }
+}
+
+// TODO: Fetch transactions for withdrawals
+async function fetchActivityDatas(depositOwner: string): Promise<Activity[]> {
+  const response = await axios.post<{
+    errors?: unknown
+    data: { activityDatas: ActivityDataResponse[] }
+  }>(ACRE_SUBGRAPH_URL, {
+    query: `query {
+            activityDatas(
+              where: {depositOwner_: {id: "${depositOwner}"}}
+            ) {
+              id
+              bitcoinTransactionId
+              events {
+                type
+              }
+              ... on Deposit {
+                amountToDeposit
+              }
+            }
+          }`,
+  })
+
+  if (response.data && response.data.errors) {
+    const errorMsg = "Failed to fetch data from Acre subgraph API."
+    console.error(errorMsg, response.data.errors)
+    throw new Error(errorMsg)
+  }
+
+  return response.data.data.activityDatas.map(mapToActivity)
+}
+
+export const subgraphAPI = {
+  fetchActivityDatas,
+}
--- dapp/src/vite-env.d.ts
+++ dapp/src/vite-env.d.ts
@@ -5,6 +5,9 @@ interface ImportMetaEnv {
   readonly VITE_ETH_HOSTNAME_HTTP: string
   readonly VITE_REFERRAL: number
   readonly VITE_DEFENDER_RELAYER_WEBHOOK_URL: string
+  readonly VITE_SUBGRAPH_LOCALHOST_URL: string
+  readonly VITE_SUBGRAPH_ID: string
+  readonly VITE_SUBGRAPH_API_KEY: string
 }
 
 interface ImportMeta {
