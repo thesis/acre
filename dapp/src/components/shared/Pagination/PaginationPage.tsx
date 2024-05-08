0ae8469e387e65d800008c4b5e1d00c20c05dd86
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-05-08 22:12:58 +0200

    Fix `Pagination` content shift
    Fixed content shift adding height animation of `PaginationPage` with use
    of `useSize` hook.

--- dapp/src/components/shared/Pagination/PaginationPage.tsx
+++ dapp/src/components/shared/Pagination/PaginationPage.tsx
@@ -1,25 +1,26 @@
-import { StackProps, Stack } from "@chakra-ui/react"
+import { StackProps, Stack, Box } from "@chakra-ui/react"
 import { AnimatePresence, Transition, Variants, motion } from "framer-motion"
 import React from "react"
+import { useSize } from "@chakra-ui/react-use-size"
 import { usePagination } from "./PaginationContext"
 
-const pageTransition: Transition = {
+const transition: Transition = {
   type: "spring",
   duration: 0.75,
 }
 
-const pageVariants: Variants = {
+const variants: Variants = {
   enter: (direction: "left" | "right") => ({
-    x: direction === "right" ? "150%" : "-150%",
-    transition: pageTransition,
+    x: direction === "right" ? "120%" : "-120%",
+    transition,
   }),
   center: {
     x: 0,
-    transition: pageTransition,
+    transition,
   },
   exit: (direction: "left" | "right") => ({
-    x: direction === "left" ? "150%" : "-150%",
-    transition: pageTransition,
+    x: direction === "left" ? "120%" : "-120%",
+    transition,
   }),
 }
 
@@ -31,22 +32,28 @@ function PaginationPage(props: PaginationPageProps) {
   const { children, ...restProps } = props
   const { currentPage, direction, pageData } = usePagination()
 
+  const ref = React.useRef<HTMLDivElement>(null)
+  const { height } = useSize(ref) ?? { height: 0 }
+
   return (
-    <AnimatePresence mode="popLayout" initial={false} custom={direction}>
-      <Stack
-        as={motion.div}
-        layout
-        custom={direction}
-        key={currentPage}
-        variants={pageVariants}
-        animate="center"
-        initial="enter"
-        exit="exit"
-        {...restProps}
-      >
-        {children(pageData)}
-      </Stack>
-    </AnimatePresence>
+    <Box as={motion.div} layout animate={{ height, transition }}>
+      <Box ref={ref}>
+        <AnimatePresence mode="popLayout" initial={false}>
+          <Stack
+            as={motion.div}
+            custom={direction}
+            key={currentPage}
+            variants={variants}
+            animate="center"
+            initial="enter"
+            exit="exit"
+            {...restProps}
+          >
+            {children(pageData)}
+          </Stack>
+        </AnimatePresence>
+      </Box>
+    </Box>
   )
 }
 
