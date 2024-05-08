c5a15030777e9e8109a2b5b39dd9cb265d0eab2d
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-05-08 07:45:40 +0200

    Simplify the env variables for subgraph

--- dapp/src/utils/subgraphAPI.ts
+++ dapp/src/utils/subgraphAPI.ts
@@ -2,21 +2,17 @@ import axios from "axios"
 import { Activity, ActivityDataResponse } from "#/types"
 
 const {
-  VITE_USE_LOCALHOST_SUBGRAPH,
   VITE_SUBGRAPH_LOCALHOST_URL,
-  VITE_SUBGRAPH_ID,
-  VITE_SUBGRAPH_API_KEY,
+  VITE_SUBGRAPH_DEVELOPMENT_URL,
+  VITE_SUBGRAPH_PRODUCTION_URL,
 } = import.meta.env
 
 const ACRE_SUBGRAPH_URL = (() => {
-  switch (VITE_USE_LOCALHOST_SUBGRAPH) {
-    case "true":
-      return VITE_SUBGRAPH_LOCALHOST_URL
-    case "false":
-      return `https://gateway-testnet-arbitrum.network.thegraph.com/api/${VITE_SUBGRAPH_API_KEY}/subgraphs/id/${VITE_SUBGRAPH_ID}`
-    default:
-      return ""
-  }
+  if (import.meta.env.PROD) return VITE_SUBGRAPH_PRODUCTION_URL
+
+  if (VITE_SUBGRAPH_LOCALHOST_URL) return VITE_SUBGRAPH_LOCALHOST_URL
+
+  return VITE_SUBGRAPH_DEVELOPMENT_URL
 })()
 
 const mapToActivity = (activityData: ActivityDataResponse): Activity => {
--- dapp/src/vite-env.d.ts
+++ dapp/src/vite-env.d.ts
@@ -5,9 +5,9 @@ interface ImportMetaEnv {
   readonly VITE_ETH_HOSTNAME_HTTP: string
   readonly VITE_REFERRAL: number
   readonly VITE_DEFENDER_RELAYER_WEBHOOK_URL: string
+  readonly VITE_SUBGRAPH_DEVELOPMENT_URL: string
   readonly VITE_SUBGRAPH_LOCALHOST_URL: string
-  readonly VITE_SUBGRAPH_ID: string
-  readonly VITE_SUBGRAPH_API_KEY: string
+  readonly VITE_SUBGRAPH_PRODUCTION_URL: string
 }
 
 interface ImportMeta {
