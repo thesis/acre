65a5a8ee3330624a90607e994aa09609ecff6e8a
Jakub Nowakowski <jakub.nowakowski@thesis.co>
2024-05-09 16:39:08 +0200

    Define maxWithdraw as public function
    To match ERC4626Upgradable contract implementation we use the same name
    of the `maxWithdraw` function to correctly override it in ERC4626Fees
    contract.

--- solidity/contracts/stBTC.sol
+++ solidity/contracts/stBTC.sol
@@ -334,7 +334,7 @@ contract stBTC is ERC4626Fees, PausableOwnable {
         if (paused()) {
             return 0;
         }
-        return _maxWithdraw(owner);
+        return super.maxWithdraw(owner);
     }
 
     /// @dev Returns the maximum amount of Vault shares that can be redeemed from
