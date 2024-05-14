eed479f4ff7ca5ead98b7676050119816df475b2
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-05-14 15:17:55 +0200

    Add Dashboard's gamification placeholder
    Hid season stats

--- dapp/src/pages/DashboardPage/index.tsx
+++ dapp/src/pages/DashboardPage/index.tsx
@@ -1,9 +1,15 @@
 import React from "react"
 import { useWallet } from "#/hooks"
+import { Icon, Image, VStack } from "@chakra-ui/react"
+import gamificationPlaceholderImage from "#/assets/images/gamification-placeholder.svg"
+import { AcreLogo } from "#/assets/icons"
+import { TextMd } from "#/components/shared/Typography"
 import { PageLayout, PageLayoutColumn } from "./PageLayout"
 import DashboardCard from "./DashboardCard"
 import GrantedSeasonPassCard from "./GrantedSeasonPassCard"
 import { CurrentSeasonCard } from "./CurrentSeasonCard"
+
+// TODO: Remove placeholder image and replace with actual gamification content
 
 export default function DashboardPage() {
   const { bitcoin } = useWallet()
@@ -16,8 +22,26 @@ export default function DashboardPage() {
       </PageLayoutColumn>
 
       <PageLayoutColumn>
-        <CurrentSeasonCard />
+        <CurrentSeasonCard showSeasonStats={false} />
         <GrantedSeasonPassCard />
+      </PageLayoutColumn>
+
+      <PageLayoutColumn position="relative">
+        <VStack py={10}>
+          <Icon as={AcreLogo} w={20} h={12} />
+          <TextMd>Coming soon...</TextMd>
+        </VStack>
+        <Image
+          zIndex={-1}
+          position="absolute"
+          inset={0}
+          src={gamificationPlaceholderImage}
+          alt="Gamification placeholder"
+          filter="auto"
+          blur="6px"
+          contrast={1.05}
+          opacity={0.5}
+        />
       </PageLayoutColumn>
     </PageLayout>
   )
