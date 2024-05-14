af3fc72bb04ffefc4bc3d07df89943bcefcfeb40
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-05-14 13:35:55 +0200

    Reuse existing utility function
    Added TODO comment to restructure utils

--- dapp/src/components/shared/Pagination/PaginationStatus.tsx
+++ dapp/src/components/shared/Pagination/PaginationStatus.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { TextProps } from "@chakra-ui/react"
+import { getPaginationState } from "#/components/TransactionHistory/Table/utils"
 import { usePagination } from "../../../contexts/PaginationContext"
 import { TextSm } from "../Typography"
 
@@ -11,8 +12,11 @@ function PaginationStatus(props: PaginationStatusProps) {
   const { dataLabel = "items", ...restProps } = props
   const { page, pageSize, totalSize } = usePagination()
 
-  const rangeStart = page * pageSize + 1 // Pages are indexed from 0
-  const rangeEnd = Math.min(rangeStart + pageSize - 1, totalSize)
+  const { rowMin: rangeStart, rowMax: rangeEnd } = getPaginationState(
+    page,
+    pageSize,
+    totalSize,
+  )
 
   return (
     <TextSm {...restProps}>
