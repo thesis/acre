10ac8b55d27b3dd18c048e957f87b2b35189af3c
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-05-14 12:13:28 +0200

    Refactor to remove `Card` component

--- dapp/src/pages/DashboardPage/CurrentSeasonCard.tsx
+++ dapp/src/pages/DashboardPage/CurrentSeasonCard.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Card, CardBody, CardHeader, CardProps, Flex } from "@chakra-ui/react"
+import { StackProps, Flex, VStack, Heading } from "@chakra-ui/react"
 import { numberToLocaleString } from "#/utils"
 import { TextMd } from "#/components/shared/Typography"
 import {
@@ -10,7 +10,7 @@ import {
 import { CurrencyBalance } from "#/components/shared/CurrencyBalance"
 import { AmountType } from "#/types"
 
-type CurrentSeasonCardProps = Omit<CardProps, "position" | "pos"> & {
+type CurrentSeasonCardProps = StackProps & {
   heading: React.ReactNode
   timestamp: number
   totalJoined?: number
@@ -20,61 +20,59 @@ type CurrentSeasonCardProps = Omit<CardProps, "position" | "pos"> & {
 export function CurrentSeasonCard(props: CurrentSeasonCardProps) {
   const { heading, timestamp, totalJoined, tvl, ...restProps } = props
   return (
-    <Card
+    <VStack
       bg="transparent"
       borderWidth={0}
-      gap={4}
+      spacing={4}
       px={5}
       py={4}
       w="full"
+      align="start"
       position="relative"
       {...restProps}
     >
       <LiveTag fontSize="sm" px={3} py={1} gap={2} />
 
-      <CardHeader
+      <Heading
         as="p"
-        p={0}
         color="gold.100"
         fontSize="2xl"
         lineHeight={1}
-        fontWeight="bold"
         letterSpacing="-0.03rem" // -0.48px
       >
         {heading}
-      </CardHeader>
+      </Heading>
 
-      <CardBody p={0} display="contents">
-        <CountdownTimer size="sm" timestamp={timestamp} />
+      <CountdownTimer size="sm" timestamp={timestamp} />
 
-        <Flex align="baseline" justify="space-between" color="white">
-          {totalJoined && (
-            <TextMd fontWeight="medium">
-              Total joined&nbsp;
-              <TextMd as="span" fontWeight="bold">
-                {numberToLocaleString(totalJoined)}
-              </TextMd>
+      <Flex align="baseline" justify="space-between" color="white">
+        {totalJoined && (
+          <TextMd fontWeight="medium">
+            Total joined&nbsp;
+            <TextMd as="span" fontWeight="bold">
+              {numberToLocaleString(totalJoined)}
             </TextMd>
-          )}
-          {!!tvl && (
-            <TextMd display="flex" fontWeight="medium">
-              TVL&nbsp;
-              <CurrencyBalance
-                as="span"
-                amount={tvl}
-                currency="bitcoin"
-                fontSize="md"
-                fontWeight="bold"
-              />
-            </TextMd>
-          )}
-        </Flex>
-      </CardBody>
+          </TextMd>
+        )}
+        {!!tvl && (
+          <TextMd display="flex" fontWeight="medium">
+            TVL&nbsp;
+            <CurrencyBalance
+              as="span"
+              amount={tvl}
+              currency="bitcoin"
+              fontSize="md"
+              fontWeight="bold"
+            />
+          </TextMd>
+        )}
+      </Flex>
+
       <SeasonCountdownSectionBackground
         position="absolute"
         inset={0}
         zIndex={-1}
       />
-    </Card>
+    </VStack>
   )
 }
