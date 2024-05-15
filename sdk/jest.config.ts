6499c9abf15e09236e5cf2db358cf77f849b8ef9
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-05-15 15:00:56 +0200

    Mock `@orangekit/sdk` to run jest test
    Reverts 553b5be6ab75587dcfc3a17c00ee25b3e19ae4dc commit. We can just
    mock the `@orangekit/sdk` module globally to run jest tests correctly
    w/o converting the JS files to CommonJS syntax.

--- sdk/jest.config.ts
+++ sdk/jest.config.ts
@@ -1,9 +1,8 @@
 import type { JestConfigWithTsJest } from "ts-jest"
 
 const jestConfig: JestConfigWithTsJest = {
-  preset: "ts-jest/presets/js-with-ts",
+  preset: "ts-jest",
   testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
-  transformIgnorePatterns: ["/node_modules/(?!@orangekit/sdk)/"],
 }
 
 export default jestConfig
