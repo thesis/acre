988459a09b643eb17fccb371081688dcf8e82c24
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-05-15 12:10:18 +0200

    Use a `createSelector` to improve selectors

--- dapp/src/store/wallet/walletSelector.ts
+++ dapp/src/store/wallet/walletSelector.ts
@@ -1,11 +1,16 @@
 import { Activity } from "#/types"
+import { createSelector } from "@reduxjs/toolkit"
 import { RootState } from ".."
 
-export const selectActivities = (state: RootState): Activity[] =>
-  Object.values(state.wallet.activities)
+export const selectActivities = createSelector(
+  (state: RootState) => state.wallet.activities,
+  (activities) => Object.values(activities),
+)
 
-export const selectActivitiesIds = (state: RootState): string[] =>
-  Object.keys(state.wallet.activities)
+export const selectActivitiesIds = createSelector(
+  (state: RootState) => state.wallet.activities,
+  (activities) => Object.keys(activities),
+)
 
 export const selectTransactions = (state: RootState): Activity[] =>
   state.wallet.transactions
