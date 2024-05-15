30526c7471f0615691f68d2a63aefd0f7937c240
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-05-15 18:20:59 +0200

    Fix `useInitializeAcreSdk` hook
    Remove unnecessary `ETHEREUM_NETWORK` param. The ethereum network is no
    longer needed to create the Acre SDK.

--- dapp/src/hooks/sdk/useInitializeAcreSdk.ts
+++ dapp/src/hooks/sdk/useInitializeAcreSdk.ts
@@ -1,5 +1,5 @@
 import { useEffect } from "react"
-import { BITCOIN_NETWORK, ETHEREUM_NETWORK } from "#/constants"
+import { BITCOIN_NETWORK } from "#/constants"
 import { logPromiseFailure } from "#/utils"
 import { useAcreContext } from "#/acre-react/hooks"
 import { LedgerLiveWalletApiBitcoinProvider } from "@acre-btc/sdk/dist/src/lib/bitcoin/providers"
@@ -17,7 +17,7 @@ export function useInitializeAcreSdk() {
         bitcoinAccountId,
         BITCOIN_NETWORK,
       )
-      await init(bitcoinProvider, ETHEREUM_NETWORK)
+      await init(bitcoinProvider)
     }
     logPromiseFailure(initSDK(btcAccount.id))
   }, [btcAccount?.id, init])
