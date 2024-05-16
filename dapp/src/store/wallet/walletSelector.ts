5d2f4146f0623c3311c13947677f3c6213b5164f
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-05-16 12:55:39 +0200

    Quick fix to display latest activity at the top of the list

--- dapp/src/store/wallet/walletSelector.ts
+++ dapp/src/store/wallet/walletSelector.ts
@@ -1,5 +1,10 @@
-import { Activity } from "#/types"
+import { createSelector } from "@reduxjs/toolkit"
 import { RootState } from ".."
 
-export const selectActivities = (state: RootState): Activity[] =>
-  state.wallet.activities
+export const selectActivities = createSelector(
+  (state: RootState) => state.wallet.activities,
+  // TODO: Temporary solution - Activities should be sorted from the latest.
+  // Let's now display the list so that the latest activity is at the top
+  // Ultimately, activities will be sorted by timestamp
+  (activities) => [...activities].reverse(),
+)
