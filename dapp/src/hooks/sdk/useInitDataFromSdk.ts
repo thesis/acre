c8c8bbbd0739e4d1273b5181c8dcc3cc21392e14
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-05-29 14:19:02 +0200

    Fetch the deposits in interval
    This was accidentally removed during the merge.

--- dapp/src/hooks/sdk/useInitDataFromSdk.ts
+++ dapp/src/hooks/sdk/useInitDataFromSdk.ts
@@ -1,20 +1,25 @@
 import { useEffect } from "react"
+import { useInterval } from "@chakra-ui/react"
+import { ONE_MINUTE_IN_SECONDS, ONE_SEC_IN_MILLISECONDS } from "#/constants"
 import { logPromiseFailure } from "#/utils"
 import { useWalletContext } from "../useWalletContext"
 import { useFetchBTCBalance } from "./useFetchBTCBalance"
 import { useFetchMinDepositAmount } from "./useFetchMinDepositAmount"
 import { useFetchDeposits } from "./useFetchDeposits"
 
+const INTERVAL_TIME = ONE_SEC_IN_MILLISECONDS * ONE_MINUTE_IN_SECONDS * 30
+
 export function useInitDataFromSdk() {
   const { btcAccount } = useWalletContext()
   const fetchDeposits = useFetchDeposits()
-
-  useFetchBTCBalance()
-  useFetchMinDepositAmount()
 
   useEffect(() => {
     if (btcAccount) {
       logPromiseFailure(fetchDeposits())
     }
   }, [btcAccount, fetchDeposits])
+
+  useFetchBTCBalance()
+  useFetchMinDepositAmount()
+  useInterval(() => logPromiseFailure(fetchDeposits()), INTERVAL_TIME)
 }
