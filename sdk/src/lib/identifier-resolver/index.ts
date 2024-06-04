bea20a562bb299a4e964b6ed830b03c645a5a8a5
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-06-04 15:07:19 +0200

    Implement `AcreIdentifierResolver`
    Implement the `toAcreIdentifier` function that returns the deposit
    owner/user identifier in the Acre protocol based on the bitcoin address.
    The function saves calculated identifier in cache so we do not need to
    recalculate it.

---
+++ sdk/src/lib/identifier-resolver/index.ts
@@ -0,0 +1,39 @@
+import { OrangeKitSdk } from "@orangekit/sdk"
+import { EthereumAddress } from "@keep-network/tbtc-v2.ts"
+import { BitcoinProvider } from "../bitcoin"
+import { ChainIdentifier } from "../contracts"
+
+type AcreUserIdentifier = ChainIdentifier
+
+type BitcoinAddress = string
+
+type AcreIdentifierCache = Map<BitcoinAddress, AcreUserIdentifier>
+
+const cache: AcreIdentifierCache = new Map()
+
+async function toAcreIdentifier(
+  bitcoinProvider: BitcoinProvider,
+  orangeKit: OrangeKitSdk,
+): Promise<AcreUserIdentifier> {
+  const bitcoinAddress = await bitcoinProvider.getAddress()
+
+  const cachedIdentifier = cache.get(bitcoinAddress)
+
+  if (cachedIdentifier) {
+    return cachedIdentifier
+  }
+
+  const identifier = EthereumAddress.from(
+    await orangeKit.predictAddress(bitcoinAddress),
+  )
+
+  cache.set(bitcoinAddress, identifier)
+
+  return identifier
+}
+
+const AcreIdentifierResolver = {
+  toAcreIdentifier,
+}
+
+export default AcreIdentifierResolver
