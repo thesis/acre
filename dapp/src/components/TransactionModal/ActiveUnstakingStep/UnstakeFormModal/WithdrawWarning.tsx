965843217f40919a560db82b0fb8ff75c1458b6b
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-06-06 11:38:18 +0200

    Use `strong` instead of `mark` tag

--- dapp/src/components/TransactionModal/ActiveUnstakingStep/UnstakeFormModal/WithdrawWarning.tsx
+++ dapp/src/components/TransactionModal/ActiveUnstakingStep/UnstakeFormModal/WithdrawWarning.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Highlight } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 import { CurrencyType } from "#/types"
 import { MINIMUM_BALANCE } from "#/constants"
 import { formatSatoshiAmount, getCurrencyByType } from "#/utils"
@@ -36,9 +36,9 @@ function WithdrawWarning({
         colorIcon="red.400"
       >
         <TextMd pr={5}>
-          <Highlight query={minimumBalanceText} styles={{ fontWeight: "bold" }}>
-            {`The new balance is below the required minimum of ${minimumBalanceText}. Withdrawing  your funds will result in the loss of your current rewards.`}
-          </Highlight>
+          The new balance is below the required minimum of
+          <Box as="strong"> {minimumBalanceText}.</Box> Withdrawing your funds
+          will result in the loss of your current rewards.
         </TextMd>
       </CardAlert>
     )
@@ -47,9 +47,9 @@ function WithdrawWarning({
   return (
     <CardAlert>
       <TextMd>
-        <Highlight query={minimumBalanceText} styles={{ fontWeight: "bold" }}>
-          {`A minimum balance of ${minimumBalanceText} is required to keep all rewards active.`}
-        </Highlight>
+        A minimum balance of
+        <Box as="strong"> {minimumBalanceText}</Box> is required to keep all
+        rewards active.
       </TextMd>
     </CardAlert>
   )
