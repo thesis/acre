51cf44acfeda08a7707b76ac6b372ca188e006ae
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-06-11 14:51:53 +0200

    Update partner logos

--- dapp/src/assets/images/partner-logos/index.ts
+++ dapp/src/assets/images/partner-logos/index.ts
@@ -2,3 +2,4 @@ export { default as baseLogo } from "#/assets/images/partner-logos/base-logo.svg
 export { default as thresholdLogo } from "#/assets/images/partner-logos/threshold-logo.svg"
 export { default as ledgerLogo } from "#/assets/images/partner-logos/ledger-logo.svg"
 export { default as wormholeLogo } from "#/assets/images/partner-logos/wormhole-logo.svg"
+export { default as mezoLogo } from "#/assets/images/partner-logos/mezo-logo.svg"
