9807a88fe480a53f356c9bb635b9e2d81f7e2bc8
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-06-13 13:01:48 +0200

    Remove the unneeded component
    After integration with OrangeKit `UnsupportedBitcoinAddressModal` is not needed in the stake/unstake flow. The account will not be connected when the address type is not supported. Therefore, the use of this modal should be done differently. Let's remove its use for now and in the next steps we should define how we want to handle it.

--- dapp/src/components/TransactionModal/ModalContentWrapper.tsx
+++ dapp/src/components/TransactionModal/ModalContentWrapper.tsx
@@ -2,38 +2,23 @@ import {
   useActionFlowStatus,
   useActionFlowTokenAmount,
   useActionFlowType,
-  useRequestBitcoinAccount,
-  useWallet,
 } from "#/hooks"
 import { PROCESS_STATUSES } from "#/types"
-import { isSupportedBTCAddressType } from "#/utils"
 import React from "react"
 import ActionFormModal from "./ActionFormModal"
 import ErrorModal from "./ErrorModal"
 import LoadingModal from "./LoadingModal"
 import ResumeModal from "./ResumeModal"
 import SuccessModal from "./SuccessModal"
-import UnsupportedBitcoinAddressModal from "./UnsupportedBitcoinAddressModal"
 
 export default function ModalContentWrapper({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const { address } = useWallet()
-  const { account, requestAccount: requestBitcoinAccount } =
-    useRequestBitcoinAccount()
   const status = useActionFlowStatus()
   const type = useActionFlowType()
   const tokenAmount = useActionFlowTokenAmount()
-
-  if (!address || !isSupportedBTCAddressType(address))
-    return (
-      <UnsupportedBitcoinAddressModal
-        account={account}
-        requestAccount={requestBitcoinAccount}
-      />
-    )
 
   if (!tokenAmount) return <ActionFormModal type={type} />
 
