0ec8c7e9353982c6761f05bc9bae8a8d3bb271a9
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-06-14 09:31:06 +0200

    Implement custom `RedeemerProxy`
    The `OrangeKitTbtcRedeemerProxy` requests redemption using the orangekit
    SDK. It builds necessary data for the orangekit safe transaction and
    sends transaction using the orangeKit.

---
+++ sdk/src/lib/redeemer-proxy.ts
@@ -0,0 +1,85 @@
+import {
+  ChainIdentifier,
+  Hex,
+  RedeemerProxy as TbtcRedeemerProxy,
+} from "@keep-network/tbtc-v2.ts"
+import { OrangeKitSdk } from "@orangekit/sdk"
+import { AcreContracts } from "./contracts"
+import { BitcoinProvider } from "./bitcoin"
+
+export default class OrangeKitTbtcRedeemerProxy implements TbtcRedeemerProxy {
+  #contracts: AcreContracts
+
+  #orangeKitSdk: OrangeKitSdk
+
+  #account: {
+    publicKey: string
+    bitcoinAddress: string
+    ethereumAddress: ChainIdentifier
+  }
+
+  #bitcoinProvider: BitcoinProvider
+
+  #sharesAmount: bigint
+
+  static init(
+    contracts: AcreContracts,
+    orangeKitSdk: OrangeKitSdk,
+    account: {
+      publicKey: string
+      bitcoinAddress: string
+      ethereumAddress: ChainIdentifier
+    },
+    bitcoinProvider: BitcoinProvider,
+    sharesAmount: bigint,
+  ) {
+    return new OrangeKitTbtcRedeemerProxy(
+      contracts,
+      orangeKitSdk,
+      account,
+      bitcoinProvider,
+      sharesAmount,
+    )
+  }
+
+  private constructor(
+    contracts: AcreContracts,
+    orangeKitSdk: OrangeKitSdk,
+    account: {
+      publicKey: string
+      bitcoinAddress: string
+      ethereumAddress: ChainIdentifier
+    },
+    bitcoinProvider: BitcoinProvider,
+    sharesAmount: bigint,
+  ) {
+    this.#contracts = contracts
+    this.#orangeKitSdk = orangeKitSdk
+    this.#account = account
+    this.#bitcoinProvider = bitcoinProvider
+    this.#sharesAmount = sharesAmount
+  }
+
+  redeemerAddress(): ChainIdentifier {
+    return this.#account.ethereumAddress
+  }
+
+  async requestRedemption(redemptionData: Hex): Promise<Hex> {
+    const safeTxData = this.#contracts.stBTC.encodeApproveAndCallFunctionData(
+      this.#contracts.bitcoinRedeemer.getChainIdentifier(),
+      this.#sharesAmount,
+      redemptionData,
+    )
+
+    const transactionHash = await this.#orangeKitSdk.sendTransaction(
+      `0x${this.#contracts.stBTC.getChainIdentifier().identifierHex}`,
+      "0x0",
+      safeTxData.toPrefixedString(),
+      this.#account.bitcoinAddress,
+      this.#account.publicKey,
+      (message: string) => this.#bitcoinProvider.signMessage(message),
+    )
+
+    return Hex.from(transactionHash)
+  }
+}
