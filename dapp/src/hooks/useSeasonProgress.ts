95c3f2b2536e746140eb1c430439c29817bace6e
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-06-17 18:10:51 +0200

    Adjust season progress percentage calculations

--- dapp/src/hooks/useSeasonProgress.ts
+++ dapp/src/hooks/useSeasonProgress.ts
@@ -3,7 +3,8 @@ import { useTotalAssets } from "./store"
 
 export const useSeasonProgress = () => {
   const totalAssets = useTotalAssets()
-  const progress = Math.floor(Number(totalAssets / SEASON_CAP) * 100)
+  const dividingResult = Number((totalAssets * 100n) / SEASON_CAP) / 100
+  const progress = Math.floor(dividingResult * 100)
 
   return { progress, totalAssets }
 }
