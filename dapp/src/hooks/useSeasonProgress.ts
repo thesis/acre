889f72d67e7f8fa8e293cdff44c2b07e05118118
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-06-18 22:04:53 +0200

    Refactor `useSeasonProgress` hook
    Covered edge case

--- dapp/src/hooks/useSeasonProgress.ts
+++ dapp/src/hooks/useSeasonProgress.ts
@@ -4,7 +4,11 @@ import { useTotalAssets } from "./store"
 export const useSeasonProgress = () => {
   const totalAssets = useTotalAssets()
   const dividingResult = Number((totalAssets * 100n) / SEASON_CAP) / 100
-  const progress = Math.floor(dividingResult * 100)
 
-  return { progress, totalAssets }
+  const isSeasonCapExceeded = totalAssets > SEASON_CAP
+
+  const progress = isSeasonCapExceeded ? 100 : Math.floor(dividingResult * 100)
+  const value = isSeasonCapExceeded ? SEASON_CAP : totalAssets
+
+  return { progress, value }
 }
