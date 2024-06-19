52da2430dc4cf014926d026e91e02931a8010d5c
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-06-19 21:43:01 +0200

    Update `ConnectWallet` component UI

--- dapp/src/components/Header/ConnectWallet.tsx
+++ dapp/src/components/Header/ConnectWallet.tsx
@@ -1,5 +1,13 @@
 import React from "react"
-import { Button, HStack, Icon, Tooltip } from "@chakra-ui/react"
+import {
+  Button,
+  HStack,
+  Icon,
+  IconButton,
+  StackDivider,
+  Tooltip,
+  useClipboard,
+} from "@chakra-ui/react"
 import { useModal, useWallet } from "#/hooks"
 import { CurrencyBalance } from "#/components/shared/CurrencyBalance"
 import { TextMd } from "#/components/shared/Typography"
@@ -7,6 +15,7 @@ import { BitcoinIcon } from "#/assets/icons"
 import { isSupportedBTCAddressType, truncateAddress } from "#/utils"
 import { AnimatePresence, motion, Variants } from "framer-motion"
 import { MODAL_TYPES } from "#/types"
+import { IconCopy, IconLogout, IconWallet } from "@tabler/icons-react"
 
 const containerVariants: Variants = {
   hidden: { opacity: 0, y: -48 },
@@ -27,6 +36,7 @@ const getCustomDataByAccount = (
 export default function ConnectWallet() {
   const { isConnected, address, balance } = useWallet()
   const { openModal } = useModal()
+  const { hasCopied, onCopy } = useClipboard(address ?? "")
 
   const customDataBtcAccount = getCustomDataByAccount(address)
 
@@ -52,35 +62,80 @@ export default function ConnectWallet() {
         >
           {isBitcoinAddressSupported && (
             <HStack display={{ base: "none", md: "flex" }}>
-              <TextMd color="grey.500">Balance</TextMd>
               <CurrencyBalance currency="bitcoin" amount={balance} />
+              <Icon as={IconWallet} boxSize={5} />
             </HStack>
           )}
-          <Tooltip
-            fontSize="xs"
-            label={
-              isBitcoinAddressSupported
-                ? "Choose account"
-                : "Click to choose account. Legacy or Native Segwit only."
-            }
-            maxW="11.25rem" // 180px
-            textAlign="center"
-            color="gold.200"
-            px={2}
-            py={1}
+
+          <HStack
+            as={motion.button}
+            whileHover="expanded"
+            initial="collapsed"
+            animate="collapsed"
+            rounded="full"
+            bg="gold.200"
+            spacing={0}
+            px={1}
+            overflow="hidden"
           >
-            <Button
-              fontWeight="medium"
-              variant="card"
-              iconSpacing={3}
-              pl={2}
-              colorScheme={customDataBtcAccount.colorScheme}
-              leftIcon={<Icon as={BitcoinIcon} boxSize={6} color="brand.400" />}
-              onClick={handleConnectWallet}
+            <HStack
+              as={motion.div}
+              variants={{
+                expanded: { paddingRight: 4 },
+                collapsed: { paddingRight: 16 },
+              }}
+              spacing={3}
+              pl={1}
+              py={2}
             >
-              {customDataBtcAccount.text}
-            </Button>
-          </Tooltip>
+              <Icon as={BitcoinIcon} boxSize={6} color="brand.400" />
+              <TextMd>{customDataBtcAccount.text}</TextMd>
+            </HStack>
+
+            <HStack
+              as={motion.div}
+              variants={{
+                expanded: { width: "auto" },
+                collapsed: { width: 0 },
+              }}
+              spacing={1}
+              divider={<StackDivider borderColor="gold.500" />}
+            >
+              <Tooltip
+                fontSize="xs"
+                label={hasCopied ? "Address copied" : "Copy"}
+                color="gold.200"
+                px={3}
+                py={2}
+                closeOnClick={false}
+              >
+                <IconButton
+                  variant="ghost"
+                  aria-label="Copy"
+                  icon={<Icon as={IconCopy} boxSize={5} />}
+                  px={2}
+                  boxSize={5}
+                  onClick={onCopy}
+                />
+              </Tooltip>
+
+              <Tooltip
+                fontSize="xs"
+                label="Disconnect"
+                color="gold.200"
+                px={3}
+                py={2}
+              >
+                <IconButton
+                  variant="ghost"
+                  aria-label="Disconnect"
+                  icon={<Icon as={IconLogout} boxSize={5} />}
+                  px={2}
+                  boxSize={5}
+                />
+              </Tooltip>
+            </HStack>
+          </HStack>
         </HStack>
       ) : (
         <Button
