4e8f9f6fe81af1cedbd5a104c09c10126255a1b7
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-06-21 16:40:37 +0200

    Refactor `parseOrangeKitConnectionError` utility
    Added `isWalletConnectionRejectedError` error type checker. Ideally this
    funtion should be provided by the OrangeKit library itself.

--- dapp/src/utils/orangeKit.ts
+++ dapp/src/utils/orangeKit.ts
@@ -5,19 +5,27 @@ import {
 } from "@orangekit/react"
 import { Connector } from "wagmi"
 
+const isWalletConnectionRejectedError = (error: unknown) => {
+  // TODO: Get error type checker from OrangeKit library
+  const { cause } = error as { cause: { code: number } }
+  return cause.code === 4001
+}
+
 const isConnectedStatus = (status: string) => status === "connected"
 
 const isOrangeKitConnector = (connector?: Connector) =>
   connector?.type === "orangekit"
 
-const parseOrangeKitConnectionError = (error: unknown) => {
-  let errorData: ConnectionErrorData = {
-    title: "Something went wrong...",
-    description: "We encountered an error. Please try again.",
+const parseOrangeKitConnectionError = (error: unknown): ConnectionErrorData => {
+  if (isWalletConnectionRejectedError(error)) {
+    return {
+      title: "Wallet connection rejected.",
+      description: "If you encountered an error, please try again.",
+    }
   }
 
   if (isUnsupportedBitcoinAddressError(error)) {
-    errorData = {
+    return {
       title: "Not supported.",
       description:
         "Only Native Segwit, Nested Segwit or Legacy addresses supported at this time. Please try a different address or another wallet.",
@@ -25,14 +33,17 @@ const parseOrangeKitConnectionError = (error: unknown) => {
   }
 
   if (isWalletNetworkDoesNotMatchProviderChainError(error)) {
-    errorData = {
+    return {
       title: "Error!",
       description:
         "Wrong network detected in your wallet. Please choose proper network and try again.",
     }
   }
 
-  return errorData
+  return {
+    title: "Something went wrong...",
+    description: "We encountered an error. Please try again.",
+  }
 }
 
 export default {
