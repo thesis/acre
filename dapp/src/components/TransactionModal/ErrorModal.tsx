278e650861876ee6c22a6e24d38d82c0210efc04
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-06-24 15:37:02 +0200

    Add temporary Error modal for unstaking flow

--- dapp/src/components/TransactionModal/ErrorModal.tsx
+++ dapp/src/components/TransactionModal/ErrorModal.tsx
@@ -1,9 +1,10 @@
 import React from "react"
 import { ACTION_FLOW_TYPES, ActionFlowType } from "#/types"
 import StakingErrorModal from "./ActiveStakingStep/StakingErrorModal"
+import UnstakeErrorModal from "./ActiveUnstakingStep/UnstakeErrorModal"
 
 export default function ErrorModal({ type }: { type: ActionFlowType }) {
   if (type === ACTION_FLOW_TYPES.STAKE) return <StakingErrorModal />
-  // TODO: Handle the case of unstake action
+  if (type === ACTION_FLOW_TYPES.UNSTAKE) return <UnstakeErrorModal />
   return null
 }
