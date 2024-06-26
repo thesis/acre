3b36b375fb19bde16da858be6fa6fc2cfed33d21
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-06-26 17:50:22 +0200

    Hanlde redemption requested event

--- subgraph/src/bitcoin-redeemer.ts
+++ subgraph/src/bitcoin-redeemer.ts
@@ -1,10 +1,27 @@
 import { RedemptionRequested } from "../generated/BitcoinRedeemer/BitcoinRedeemer"
-import { getOrCreateWithdraw } from "./utils"
+import {
+  getOrCreateDepositOwner,
+  getOrCreateEvent,
+  getOrCreateWithdraw,
+} from "./utils"
 
 // eslint-disable-next-line import/prefer-default-export
 export function handleRedemptionRequested(event: RedemptionRequested): void {
+  const ownerEntity = getOrCreateDepositOwner(event.params.owner)
   // TODO: build withdraw id - redemption key + count.
   const id = ""
   const withdraw = getOrCreateWithdraw(id)
   // TODO: set all properties of the withdraw entity.
+  withdraw.depositOwner = ownerEntity.id
+  withdraw.amount = event.params.tbtcAmount
+
+  const redemptionRequestedEvent = getOrCreateEvent(
+    `${event.transaction.hash.toHexString()}_RedemptionRequested`,
+  )
+  redemptionRequestedEvent.activity = withdraw.id
+  redemptionRequestedEvent.timestamp = event.block.timestamp
+  redemptionRequestedEvent.type = "Initialized"
+
+  withdraw.save()
+  redemptionRequestedEvent.save()
 }
--- subgraph/src/tbtc-utils.ts
+++ subgraph/src/tbtc-utils.ts
@@ -15,7 +15,12 @@ const DEPOSIT_REVEALED_EVENT_SIGNATURE = crypto.keccak256(
   ),
 )
 
-// eslint-disable-next-line import/prefer-default-export
+const REDEMPTION_REQUESTED_EVENT_SIGNATURE = crypto.keccak256(
+  ByteArray.fromUTF8(
+    "RedemptionRequested(indexed bytes20,bytes,indexed,address,uint64,uint64,uint64)",
+  ),
+)
+
 export function findBitcoinTransactionIdFromTransactionReceipt(
   transactionReceipt: ethereum.TransactionReceipt | null,
 ): string {
@@ -51,4 +56,57 @@ export function findBitcoinTransactionIdFromTransactionReceipt(
     .slice(2)
 
   return bitcoinTransactionId
+}
+
+/**
+ * keccak256(keccak256(redeemerOutputScript) | walletPubKeyHash) + "-" + count
+ * */
+export function buildRedemptionKey(
+  redeemerOutputScript: ByteArray,
+  walletPublicKeyHash: ByteArray,
+): string {
+  const scriptHashArray = crypto.keccak256(redeemerOutputScript)
+  const data = new Uint8Array(
+    scriptHashArray.length + walletPublicKeyHash.length,
+  )
+
+  data.set(scriptHashArray, 0)
+  data.set(walletPublicKeyHash, scriptHashArray.length)
+
+  return crypto.keccak256(Bytes.fromUint8Array(data)).toHexString()
+}
+
+export function findRedemptionKeyFromReceipt(
+  transactionReceipt: ethereum.TransactionReceipt | null,
+): string {
+  const tbtcV2BridgeAddress = Address.fromBytes(
+    dataSource.context().getBytes("tbtcBridgeAddress"),
+  )
+
+  if (!transactionReceipt) {
+    throw new Error("Transaction receipt not available")
+  }
+
+  // We must cast manually to `ethereum.TransactionReceipt` otherwise
+  // AssemblyScript will fail.
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+  const receipt = transactionReceipt as ethereum.TransactionReceipt
+
+  const redemptionRequestedLog = findLogByEventSignatureInLogs(
+    receipt.logs,
+    REDEMPTION_REQUESTED_EVENT_SIGNATURE,
+    tbtcV2BridgeAddress,
+  )
+
+  // The wallet public key hash is first indexed param.
+  const walletPublicKeyHash = redemptionRequestedLog.topics[1]
+  const redemerOutputScript = ByteArray.fromHexString(
+    redemptionRequestedLog.data
+      .toHexString()
+      // The first 20 bytes (w/o 0x prefix) is wallet public key hash. The next
+      // 64 bytes is redeemer output script.
+      .slice(22, 64),
+  )
+
+  return buildRedemptionKey(redemerOutputScript, walletPublicKeyHash)
 }
