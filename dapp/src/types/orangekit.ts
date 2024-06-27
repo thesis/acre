d8e131be9a235df27972fc88df966a0c7ad82b48
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-06-27 15:45:31 +0200

    Resolve orangekit types issue

--- dapp/src/types/orangekit.ts
+++ dapp/src/types/orangekit.ts
@@ -1,3 +1,9 @@
 import { CreateOrangeKitConnectorFn } from "@orangekit/react/dist/src/wallet/connector"
 
 export type OrangeKitConnector = ReturnType<CreateOrangeKitConnectorFn>
+
+export type OrangeKitError = Error & {
+  cause?: {
+    code: number
+  }
+}
