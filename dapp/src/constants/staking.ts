7c5d874c6f9d1f67c966ec04adb3cd6839c19c04
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-07-08 14:21:00 +0200

    Reduce Season 1 cap to 500 BTC

--- dapp/src/constants/staking.ts
+++ dapp/src/constants/staking.ts
@@ -3,4 +3,4 @@ export const REFERRAL = import.meta.env.VITE_REFERRAL
 // TODO: Read the value from the SDK, once we expose it
 export const MINIMUM_BALANCE = BigInt(String(5e6)) // 0.05 BTC
 
-export const SEASON_CAP = BigInt(String(2e11)) // 2,000.00 BTC
+export const SEASON_CAP = BigInt(String(5e10)) // 500 BTC
