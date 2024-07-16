16a0b1ef878a2a343cd320ce3edbb41a4356afa0
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-07-16 18:55:08 +0200

    Add a skeleton for `BeehiveCard`

--- dapp/src/pages/DashboardPage/BeehiveCard.tsx
+++ dapp/src/pages/DashboardPage/BeehiveCard.tsx
@@ -7,15 +7,22 @@ import {
   CardHeader,
   CardProps,
   Highlight,
+  HStack,
   Image,
 } from "@chakra-ui/react"
 import { ArrowUpRight, MezoSignIcon } from "#/assets/icons"
-import { useModal } from "#/hooks"
+import { useIsSignedMessage, useModal } from "#/hooks"
 import { MODAL_TYPES } from "#/types"
 import mezoBeehiveCardIllustrationSrc from "#/assets/images/mezo-beehive-card-illustration.svg"
+import beehiveIllustrationSrc from "#/assets/images/beehive-illustration.svg"
+import UserDataSkeleton from "#/components/shared/UserDataSkeleton"
+
+const MARGIN = 4
 
 export default function BeehiveCard(props: CardProps) {
   const { openModal } = useModal()
+  const isSignedMessage = useIsSignedMessage()
+
   const handleOpenBeehiveModal = () => {
     openModal(MODAL_TYPES.MEZO_BEEHIVE)
   }
@@ -28,42 +35,50 @@ export default function BeehiveCard(props: CardProps) {
         </TextMd>
       </CardHeader>
 
-      <CardBody p={0} mx={-4}>
-        <Image src={mezoBeehiveCardIllustrationSrc} pl={4} mx="auto" mb={4} />
+      <CardBody p={0} mx={-MARGIN}>
+        {isSignedMessage ? (
+          <Image src={mezoBeehiveCardIllustrationSrc} pl={MARGIN} mx="auto" />
+        ) : (
+          <HStack justifyContent="space-between" pl={MARGIN}>
+            <UserDataSkeleton boxSize={24} rounded="100%" />
+            <Image src={beehiveIllustrationSrc} />
+          </HStack>
+        )}
 
-        <Card
-          borderWidth={0}
-          variant="elevated"
-          colorScheme="gold"
-          align="center"
-          px={4}
-          py={3}
-          mx={4}
-        >
-          <CardHeader p={0} mb={1}>
-            <TextMd fontWeight="semibold" color="grey.500">
-              <Highlight
-                query="Mezo"
-                styles={{ fontWeight: "bold", color: "grey.700" }}
+        <UserDataSkeleton mx={MARGIN} mt={4}>
+          <Card
+            borderWidth={0}
+            variant="elevated"
+            colorScheme="gold"
+            align="center"
+            px={4}
+            py={3}
+          >
+            <CardHeader p={0} mb={1}>
+              <TextMd fontWeight="semibold" color="grey.500">
+                <Highlight
+                  query="Mezo"
+                  styles={{ fontWeight: "bold", color: "grey.700" }}
+                >
+                  Collecting honey from Mezo
+                </Highlight>
+
+                <MezoSignIcon boxSize={5} rounded="full" ml={1} />
+              </TextMd>
+            </CardHeader>
+
+            <CardBody p={0}>
+              <Button
+                onClick={() => handleOpenBeehiveModal()}
+                variant="ghost"
+                rightIcon={<ArrowUpRight />}
+                color="brand.400"
               >
-                Collecting honey from Mezo
-              </Highlight>
-
-              <MezoSignIcon boxSize={5} rounded="full" ml={1} />
-            </TextMd>
-          </CardHeader>
-
-          <CardBody p={0}>
-            <Button
-              onClick={() => handleOpenBeehiveModal()}
-              variant="ghost"
-              rightIcon={<ArrowUpRight />}
-              color="brand.400"
-            >
-              More info
-            </Button>
-          </CardBody>
-        </Card>
+                More info
+              </Button>
+            </CardBody>
+          </Card>
+        </UserDataSkeleton>
       </CardBody>
     </Card>
   )
