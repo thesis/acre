27b706f218a230038b76ecea0157744340820bf5
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-07-17 16:06:21 +0200

    Implement printing network type and commit hash

--- dapp/vite.config.ts
+++ dapp/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { nodePolyfills } from "vite-plugin-node-polyfills"
-
 import { resolve } from "path"
+import * as child from "child_process"
+
+const latestCommitHash = JSON.stringify(
+  child.execSync("git rev-parse --short HEAD").toString(),
+)
 
 export default defineConfig({
   resolve: {
@@ -11,4 +15,7 @@ export default defineConfig({
     },
   },
   plugins: [nodePolyfills(), react()],
+  define: {
+    "import.meta.env.VITE_LATEST_COMMIT_HASH": latestCommitHash,
+  },
 })
