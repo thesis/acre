fe94706657a222541d859ab9bae9ecbd77dac022
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-07-18 22:26:47 +0200

    Disable route change animation
    Animation was glitchy and made layout difficult to compose with
    `MobileModeBanner`

--- dapp/src/components/Layout.tsx
+++ dapp/src/components/Layout.tsx
@@ -1,54 +1,21 @@
-import React, { useState } from "react"
-import { AnimatePresence, motion, Variants } from "framer-motion"
-import { useLocation, useOutlet } from "react-router-dom"
+import React from "react"
+import { Outlet } from "react-router-dom"
 import { Flex } from "@chakra-ui/react"
 import DocsDrawer from "./DocsDrawer"
 import Header from "./Header"
 import ModalRoot from "./ModalRoot"
 import Sidebar from "./Sidebar"
 
-const MotionFlex = motion(Flex)
-
-const wrapperVariants: Variants = {
-  in: { opacity: 0, y: 48 },
-  out: { opacity: 0, y: -48 },
-  visible: { opacity: 1, y: 0 },
-}
-
-// This tricky component makes Outlet persistent so React and Framer Motion can
-// distinguish wheather it should be rerendered between routes.
-// Ref: https://github.com/remix-run/react-router/discussions/8008#discussioncomment-1280897
-function PersistentOutlet() {
-  const [outlet] = useState(useOutlet())
-  return outlet
-}
-
 function Layout() {
-  const location = useLocation()
-
   return (
-    <>
-      <AnimatePresence mode="popLayout">
-        <MotionFlex
-          as="main"
-          flexFlow="column"
-          key={location.pathname}
-          variants={wrapperVariants}
-          transition={{ type: "spring", damping: 12, stiffness: 120 }}
-          initial="in"
-          animate="visible"
-          exit="out"
-        >
-          <Header />
-
-          <PersistentOutlet />
-        </MotionFlex>
-      </AnimatePresence>
+    <Flex flexFlow="column">
+      <Header />
+      <Outlet />
 
       <Sidebar />
       <DocsDrawer />
       <ModalRoot />
-    </>
+    </Flex>
   )
 }
 
