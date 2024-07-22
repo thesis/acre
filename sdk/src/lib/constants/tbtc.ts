67f1d549af9203eb286181e792c4b899d3b07076
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-07-22 17:03:10 +0200

    Set default bitcoin recovery address for testnet and mainnet

--- sdk/src/lib/constants/tbtc.ts
+++ sdk/src/lib/constants/tbtc.ts
@@ -4,8 +4,8 @@ const NETWORK_TO_TBTC_DEPOSIT_BITCOIN_RECOVERY_ADDRESS: Record<
   BitcoinNetwork,
   string
 > = {
-  mainnet: "TODO",
-  testnet: "TODO",
+  mainnet: "bc1qhv2fuyy3pessde3cdgtlrpz3mufytf8en2wr28",
+  testnet: "tb1qtzhpl9sc3t0s2yhq5sevl3qklfduxym3twcn6v",
 }
 
 export default {
