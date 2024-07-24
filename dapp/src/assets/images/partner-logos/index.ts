0599044b66b4c22fba6effaea3e75b4d954a85ca
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-07-24 12:32:52 +0200

    Adjust `CurrentSeasonSection` component

--- dapp/src/assets/images/partner-logos/index.ts
+++ dapp/src/assets/images/partner-logos/index.ts
@@ -1,2 +1,3 @@
 export { default as thresholdLogo } from "#/assets/images/partner-logos/threshold-logo.svg"
 export { default as mezoLogo } from "#/assets/images/partner-logos/mezo-logo.svg"
+export { default as mezoLogoColor } from "#/assets/images/partner-logos/mezo-logo-color.svg"
--- dapp/src/pages/LandingPage/components/CurrentSeasonSection.tsx
+++ dapp/src/pages/LandingPage/components/CurrentSeasonSection.tsx
@@ -1,13 +1,20 @@
 import React from "react"
-import { Box, VStack, Flex } from "@chakra-ui/react"
+import {
+  Box,
+  VStack,
+  Card,
+  CardHeader,
+  CardBody,
+  Image,
+} from "@chakra-ui/react"
 import ProgressBar from "#/components/shared/ProgressBar"
 import { CurrencyBalance } from "#/components/shared/CurrencyBalance"
 import { H3, TextLg, TextMd, TextXl } from "#/components/shared/Typography"
-import { BENEFITS, SEASON_CAP } from "#/constants"
+import { SEASON_CAP } from "#/constants"
 import { LiveTag } from "#/components/shared/LiveTag"
 import { SeasonSectionBackground } from "#/components/shared/SeasonSectionBackground"
 import { useSeasonProgress } from "#/hooks"
-import BenefitCard from "./BenefitCard"
+import { mezoLogoColor } from "#/assets/images/partner-logos"
 
 export default function CurrentSeasonSection() {
   const { progress: seasonProgress, value: seasonTotalAssets } =
@@ -18,7 +25,7 @@ export default function CurrentSeasonSection() {
       <VStack
         spacing={0}
         px={{ base: 6, xl: 0 }}
-        pb={12}
+        pb={8}
         textAlign="center"
         color="grey.700"
       >
@@ -88,6 +95,8 @@ export default function CurrentSeasonSection() {
           />
         </TextXl>
 
+        {/* TODO: Uncomment in post-launch phases */}
+        {/* 
         <Flex
           flexDirection={{ base: "column", xl: "row" }}
           gap={5}
@@ -105,8 +114,26 @@ export default function CurrentSeasonSection() {
             >
               <TextMd>{description}</TextMd>
             </BenefitCard>
-          ))}
+          ))} 
+          
         </Flex>
+        */}
+
+        <Card p={6} w="full" maxW="landing_page_content_width" mx="auto">
+          <CardHeader p={0} mb={6}>
+            <TextMd fontWeight="semibold">Season partner</TextMd>
+          </CardHeader>
+          <CardBody p={0} as={VStack} spacing={6}>
+            <Image
+              src={mezoLogoColor}
+              maxW="16.5rem" // 264px
+            />
+            <TextMd fontWeight="medium" color="grey.500">
+              Mezo is the economic layer for Bitcoin with a mission to activate
+              a trillion dollar opportunity.
+            </TextMd>
+          </CardBody>
+        </Card>
       </VStack>
 
       <SeasonSectionBackground
