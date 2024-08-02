72f98ac226caf33e60ad3564bf736b9a08d2d35e
Jakub Nowakowski <jakub.nowakowski@thesis.co>
2024-08-02 14:17:34 +0200

    Update MezoAllocator to match the new partial withdrawal logic from Mezo Portal
    Mezo Portal contract updated the logic for full and partial withdrawals
    in https://github.com/thesis/mezo-portal/pull/1019,
    Here we update the MezoAllocator contract to match the new logic.

--- solidity/contracts/MezoAllocator.sol
+++ solidity/contracts/MezoAllocator.sol
@@ -16,10 +16,17 @@ interface IMezoPortal {
     ///         token. Some tokens can be deposited but can not be locked - in
     ///         that case the unlockAt is the block timestamp of when the deposit
     ///         was created. The same is true for tokens that can be locked but
-    ///         the depositor decided not to lock them.
+    ///         the depositor decided not to lock them. Some deposits can mint
+    ///         a receipt tokens against them: receiptMinted is the amount of
+    ///         receipt tokens minted against a deposit, while feeOwed is the
+    ///         fee owed by the deposit to Portal, and the lastFeeIntegral is
+    ///         the last updated value of the fee integral.
     struct DepositInfo {
         uint96 balance;
         uint32 unlockAt;
+        uint96 receiptMinted;
+        uint96 feeOwed;
+        uint88 lastFeeIntegral;
     }
 
     /// @notice Deposit and optionally lock tokens for the given period.
@@ -31,16 +38,50 @@ interface IMezoPortal {
     /// @param lockPeriod lock period in seconds, 0 to not lock the deposit
     function deposit(address token, uint96 amount, uint32 lockPeriod) external;
 
-    /// @notice Withdraw deposited tokens.
+    /// @notice Withdraws all deposited tokens.
+    ///
     ///         Deposited lockable tokens can be withdrawn at any time if
     ///         there is no lock set on the deposit or the lock period has passed.
     ///         There is no way to withdraw locked deposit. Tokens that are not
-    ///         lockable can be withdrawn at any time. Deposit can be withdrawn
-    ///         partially.
+    ///         lockable can be withdrawn at any time.
+    ///
+    ///         Deposits for which receipt tokens were minted and not fully
+    ///         repaid can not be withdrawn even if the lock expired. Repaying
+    ///         all receipt tokens is a must to withdraw the deposit. Upon
+    ///         withdrawing a deposit for which the receipt tokens were minted,
+    ///         the fee is collected based on the annual fee and the amount
+    ///         of minted receipt tokens.
+    ///
+    ///         This function withdraws all deposited tokens. For partial
+    ///         withdrawals, use `withdrawPartially`.
     /// @param token deposited token address
     /// @param depositId id of the deposit
-    /// @param amount amount of the token to be withdrawn from the deposit
-    function withdraw(address token, uint256 depositId, uint96 amount) external;
+    function withdraw(address token, uint256 depositId) external;
+
+    /// @notice Withdraws part of the deposited tokens.
+    ///
+    ///         Deposited lockable tokens can be withdrawn at any time if
+    ///         there is no lock set on the deposit or the lock period has passed.
+    ///         There is no way to withdraw locked deposit. Tokens that are not
+    ///         lockable can be withdrawn at any time.
+    ///
+    ///         Deposits for which receipt tokens were minted and fully repaid
+    ///         can not be partially withdrawn even if the lock expired.
+    ///         Repaying all receipt tokens is a must to partially withdraw the
+    ///         deposit. If the fee for receipt tokens minted is non-zero, the
+    ///         deposit can not be partially withdrawn and only a full
+    ///         withdrawal is possible.
+    ///
+    ///         This function allows only for partial withdrawals. For full
+    ///         withdrawals, use `withdraw`.
+    /// @param token deposited token address
+    /// @param depositId id of the deposit
+    /// @param amount the amount to be withdrawn
+    function withdrawPartially(
+        address token,
+        uint256 depositId,
+        uint96 amount
+    ) external;
 
     /// @notice The number of deposits created. Includes the deposits that
     ///         were fully withdrawn. This is also the identifier of the most
@@ -150,7 +191,7 @@ contract MezoAllocator is IDispatcher, Ownable2StepUpgradeable {
         if (depositBalance > 0) {
             // Free all Acre's tBTC from MezoPortal before creating a new deposit.
             // slither-disable-next-line reentrancy-no-eth
-            mezoPortal.withdraw(address(tbtc), depositId, depositBalance);
+            mezoPortal.withdraw(address(tbtc), depositId);
         }
 
         // Fetch unallocated tBTC from stBTC contract.
@@ -185,7 +226,7 @@ contract MezoAllocator is IDispatcher, Ownable2StepUpgradeable {
         if (msg.sender != address(stbtc)) revert CallerNotStbtc();
 
         emit DepositWithdrawn(depositId, amount);
-        mezoPortal.withdraw(address(tbtc), depositId, uint96(amount));
+        mezoPortal.withdrawPartially(address(tbtc), depositId, uint96(amount));
         // slither-disable-next-line reentrancy-benign
         depositBalance -= uint96(amount);
         tbtc.safeTransfer(address(stbtc), amount);
@@ -201,7 +242,7 @@ contract MezoAllocator is IDispatcher, Ownable2StepUpgradeable {
 
         emit DepositReleased(depositId, amount);
         depositBalance = 0;
-        mezoPortal.withdraw(address(tbtc), depositId, amount);
+        mezoPortal.withdraw(address(tbtc), depositId);
         tbtc.safeTransfer(address(stbtc), tbtc.balanceOf(address(this)));
     }
 
