d667eb6a59ff08a549d46697fa6f331dfc96697c
Jakub Nowakowski <jakub.nowakowski@thesis.co>
2024-08-02 14:19:30 +0200

    Update MezoPortalStub to match new IMezoPortal interface
    The interface has been updated to include a new function, `withdrawPartial`,
    which allows users to withdraw a portion of their funds from the contract.
    This change requires updating the MezoPortalStub contract to match the new interface.

--- solidity/contracts/test/MezoPortalStub.sol
+++ solidity/contracts/test/MezoPortalStub.sol
@@ -4,12 +4,21 @@ pragma solidity 0.8.24;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract MezoPortalStub {
+import {IMezoPortal} from "../MezoAllocator.sol";
+
+contract MezoPortalStub is IMezoPortal {
     using SafeERC20 for IERC20;
 
     uint256 public depositCount;
 
-    function withdraw(
+    function withdraw(address token, uint256 depositId) external {
+        IERC20(token).safeTransfer(
+            msg.sender,
+            IERC20(token).balanceOf(address(this))
+        );
+    }
+
+    function withdrawPartially(
         address token,
         uint256 depositId,
         uint96 amount
@@ -26,10 +35,14 @@ contract MezoPortalStub {
         address depositor,
         address token,
         uint256 depositId
-    ) external view returns (uint96 balance, uint256 unlockAt) {
-        return (
-            uint96(IERC20(token).balanceOf(address(this))),
-            block.timestamp
-        );
+    ) external view returns (IMezoPortal.DepositInfo memory) {
+        return
+            IMezoPortal.DepositInfo({
+                balance: uint96(IERC20(token).balanceOf(address(this))),
+                unlockAt: uint32(block.timestamp),
+                receiptMinted: uint96(0),
+                feeOwed: uint96(0),
+                lastFeeIntegral: uint88(0)
+            });
     }
 }
