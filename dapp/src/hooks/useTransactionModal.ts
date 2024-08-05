fb57fba54eaf902760a87d9be38d07a4b058a954
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-08-05 15:16:30 +0200

    Do not close the Unstake modal on esc button
    To be able to cancel the withdrawal flow when a user closes the modal.

--- dapp/src/hooks/useTransactionModal.ts
+++ dapp/src/hooks/useTransactionModal.ts
@@ -1,4 +1,4 @@
-import { ActionFlowType, MODAL_TYPES } from "#/types"
+import { ACTION_FLOW_TYPES, ActionFlowType, MODAL_TYPES } from "#/types"
 import { useCallback } from "react"
 import { useModal } from "./useModal"
 
@@ -6,6 +6,9 @@ export function useTransactionModal(type: ActionFlowType) {
   const { openModal } = useModal()
 
   return useCallback(() => {
-    openModal(MODAL_TYPES[type], { type })
+    openModal(MODAL_TYPES[type], {
+      type,
+      closeOnEsc: type !== ACTION_FLOW_TYPES.UNSTAKE,
+    })
   }, [openModal, type])
 }
