25aea8f5b36fa6cb8eae6e8fe308a8b80ec2baef
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-09-12 15:28:19 +0200

    Hide points estimations behind feature flag

--- dapp/src/components/TransactionModal/ActiveStakingStep/StakeFormModal/index.tsx
+++ dapp/src/components/TransactionModal/ActiveStakingStep/StakeFormModal/index.tsx
@@ -5,6 +5,7 @@ import { useMinDepositAmount, useWallet } from "#/hooks"
 import { FormSubmitButton } from "#/components/shared/Form"
 import { ACTION_FLOW_TYPES, BaseFormProps } from "#/types"
 import { fixedPointNumberToString, getCurrencyByType } from "#/utils"
+import { featureFlags } from "#/constants"
 import StakeDetails from "./StakeDetails"
 import AcrePointsRewardEstimation from "./AcrePointsRewardEstimation"
 
@@ -29,7 +30,7 @@ function StakeFormModal({
       onSubmitForm={onSubmitForm}
       withMaxButton={false}
     >
-      <AcrePointsRewardEstimation />
+      {featureFlags.ACRE_POINTS_ENABLED && <AcrePointsRewardEstimation />}
       <StakeDetails currency="bitcoin" />
       <FormSubmitButton mt={10}>Deposit</FormSubmitButton>
     </TokenAmountForm>
