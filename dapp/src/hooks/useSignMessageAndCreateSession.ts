e32336eec604d9ea11b930ea8daf2ece45e4798c
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-09-25 14:11:27 +0200

    Disable signature verification on login

--- dapp/src/hooks/useSignMessageAndCreateSession.ts
+++ dapp/src/hooks/useSignMessageAndCreateSession.ts
@@ -1,5 +1,7 @@
 import { OrangeKitConnector } from "#/types"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { acreApi, orangeKit } from "#/utils"
+import { generateNonce } from "@orangekit/sign-in-with-wallet"
 import { useCallback } from "react"
 import { useSignMessage } from "wagmi"
 
@@ -8,43 +10,41 @@ function useSignMessageAndCreateSession() {
 
   const signMessageAndCreateSession = useCallback(
     async (connectedConnector: OrangeKitConnector, btcAddress: string) => {
-      let session = await acreApi.getSession()
-      const hasSessionAddress = "address" in session
+      // const session = await acreApi.getSession()
+      // const hasSessionAddress = "address" in session
 
-      const isSessionAddressEqual = hasSessionAddress
-        ? (session as { address: string }).address === btcAddress
-        : false
+      // const isSessionAddressEqual = hasSessionAddress
+      //   ? (session as { address: string }).address === btcAddress
+      //   : false
 
-      if (hasSessionAddress && isSessionAddressEqual) {
-        return
-      }
+      // if (hasSessionAddress && isSessionAddressEqual) {
+      //   return
+      // }
 
-      if (hasSessionAddress && !isSessionAddressEqual) {
-        // Delete session.
-        await acreApi.deleteSession()
-        // Ask for nonce to create new session.
-        session = await acreApi.getSession()
-      }
+      // if (hasSessionAddress && !isSessionAddressEqual) {
+      //   // Delete session.
+      //   await acreApi.deleteSession()
+      //   // Ask for nonce to create new session.
+      //   session = await acreApi.getSession()
+      // }
 
-      if (!("nonce" in session)) {
-        throw new Error("Session nonce not available")
-      }
+      // if (!("nonce" in session)) {
+      //   throw new Error("Session nonce not available")
+      // }
+      const nonce = generateNonce()
+      const message = orangeKit.createSignInWithWalletMessage(btcAddress, nonce)
 
-      const message = orangeKit.createSignInWithWalletMessage(
-        btcAddress,
-        session.nonce,
-      )
-
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const signedMessage = await signMessageAsync({
         message,
         connector: orangeKit.typeConversionToConnector(connectedConnector),
       })
 
-      const publicKey = await connectedConnector
-        .getBitcoinProvider()
-        .getPublicKey()
+      // const publicKey = await connectedConnector
+      //   .getBitcoinProvider()
+      //   .getPublicKey()
 
-      await acreApi.createSession(message, signedMessage, publicKey)
+      // await acreApi.createSession(message, signedMessage, publicKey)
     },
     [signMessageAsync],
   )
