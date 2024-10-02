c2f723a2165e06c46aa0cf5556902feb40ae92c8
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2024-10-02 08:01:26 +0200

    Mock session mechanism in dapp
    We want the flow to be as close to the original as possible, so here we
    simulate the session mechanism.

--- dapp/src/hooks/useSignMessageAndCreateSession.ts
+++ dapp/src/hooks/useSignMessageAndCreateSession.ts
@@ -1,9 +1,13 @@
 import { OrangeKitConnector } from "#/types"
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { acreApi, orangeKit } from "#/utils"
+import { acreApi, getExpirationDate, orangeKit } from "#/utils"
 import { generateNonce } from "@orangekit/sign-in-with-wallet"
 import { useCallback } from "react"
 import { useSignMessage } from "wagmi"
+import { ACRE_SESSION_EXPIRATION_TIME } from "#/constants"
+import useLocalStorage from "./useLocalStorage"
+
+const initialSession = { address: "", sessionId: 0 }
 
 function useSignMessageAndCreateSession() {
   const {
@@ -11,6 +15,16 @@ function useSignMessageAndCreateSession() {
     status: signMessageStatus,
     reset: resetMessageStatus,
   } = useSignMessage()
+
+  // TODO: Temporary solution to mock the session mechanism for Ledger Live App
+  // integration. To fully support the session mechanism exposed by Acre API
+  // backend we need sign message with "zero" address not fresh address. This is
+  // will be supported in future versions of Ledger Bitcoin App.
+  const [session, setSession] = useLocalStorage<{
+    address: string
+    sessionId: number
+  }>("acre.session", initialSession)
+  const { address: sessionAddress, sessionId } = session
 
   const signMessageAndCreateSession = useCallback(
     async (connectedConnector: OrangeKitConnector, btcAddress: string) => {
@@ -35,6 +49,14 @@ function useSignMessageAndCreateSession() {
       // if (!("nonce" in session)) {
       //   throw new Error("Session nonce not available")
       // }
+      if (
+        new Date(sessionId).getTime() > Date.now() &&
+        btcAddress === sessionAddress
+      ) {
+        // The session is valid no need to sign message.
+        return
+      }
+
       const nonce = generateNonce()
       const message = orangeKit.createSignInWithWalletMessage(btcAddress, nonce)
 
@@ -44,13 +66,18 @@ function useSignMessageAndCreateSession() {
         connector: orangeKit.typeConversionToConnector(connectedConnector),
       })
 
+      const newSessionId = getExpirationDate(
+        ACRE_SESSION_EXPIRATION_TIME,
+      ).getTime()
+      setSession({ address: btcAddress, sessionId: newSessionId })
+
       // const publicKey = await connectedConnector
       //   .getBitcoinProvider()
       //   .getPublicKey()
 
       // await acreApi.createSession(message, signedMessage, publicKey)
     },
-    [signMessageAsync],
+    [signMessageAsync, sessionAddress, sessionId, setSession],
   )
 
   return {
