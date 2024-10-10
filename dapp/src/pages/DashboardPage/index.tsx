91771959e6ed7a106daeb5e7c2fb03e33e61c32f
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-10-10 10:01:14 +0200

    Add a basic grid for the dashboard view for the embedded dApp

--- dapp/src/pages/DashboardPage/index.tsx
+++ dapp/src/pages/DashboardPage/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
-import { useTriggerConnectWalletModal } from "#/hooks"
+import { useIsEmbed, useTriggerConnectWalletModal } from "#/hooks"
 import { featureFlags } from "#/constants"
+import { Grid } from "@chakra-ui/react"
 import DashboardCard from "./DashboardCard"
 import { PageLayout, PageLayoutColumn } from "./PageLayout"
 // import GrantedSeasonPassCard from "./GrantedSeasonPassCard"
@@ -9,8 +10,39 @@ import { CurrentSeasonCard } from "./CurrentSeasonCard"
 import BeehiveCard from "./BeehiveCard"
 import UsefulLinks from "./UsefulLinks"
 
+const TEMPLATE_AREAS_LEDGER_LIVE = `${featureFlags.TVL_ENABLED ? '"dashboard current-season"' : ""}
+                                    ${featureFlags.BEEHIVE_COMPONENT_ENABLED ? '"dashboard beehive"' : ""}
+                                    "dashboard acre-points"
+                                    "dashboard useful-links"`
+
 export default function DashboardPage() {
+  const { isEmbed } = useIsEmbed()
+
   useTriggerConnectWalletModal()
+
+  if (isEmbed)
+    return (
+      <Grid
+        gridGap={{ base: 4, lg: 8 }}
+        gridTemplateColumns={{ base: "1fr 1fr", lg: "1fr 30%" }}
+        templateAreas={TEMPLATE_AREAS_LEDGER_LIVE}
+      >
+        <DashboardCard gridArea="dashboard" h="fit-content" />
+        {/* TODO: Uncomment in post-launch phases + add `gridArea` and update  `templateAreas` */}
+        {/* <GrantedSeasonPassCard /> */}
+        {featureFlags.TVL_ENABLED && (
+          <CurrentSeasonCard
+            showSeasonStats={false}
+            gridArea="current-season"
+          />
+        )}
+        {featureFlags.BEEHIVE_COMPONENT_ENABLED && (
+          <BeehiveCard gridArea="beehive" />
+        )}
+        <AcrePointsCard gridArea="acre-points" />
+        <UsefulLinks gridArea="useful-links" />
+      </Grid>
+    )
 
   return (
     <PageLayout>
