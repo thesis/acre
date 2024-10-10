791ce853bbc166bb09de9aebc86472f0b9eecdd5
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-10-10 13:41:40 +0200

    Update maximum width for dApp

--- dapp/src/components/Layout.tsx
+++ dapp/src/components/Layout.tsx
@@ -1,22 +1,31 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
 import { Flex } from "@chakra-ui/react"
-import { useMobileMode } from "#/hooks"
+import { useIsEmbed, useMobileMode } from "#/hooks"
 import DocsDrawer from "./DocsDrawer"
 import Header from "./Header"
 import ModalRoot from "./ModalRoot"
 import Sidebar from "./Sidebar"
 import MobileModeBanner from "./MobileModeBanner"
 
-const PAGE_MAX_WIDTH = "110rem" // 1760px -> content width + x-axis padding
+const PAGE_MAX_WIDTH = {
+  standalone: "110rem", // 1760px
+  embed: "68rem", // 1088px
+}
 
 function Layout() {
   const isMobileMode = useMobileMode()
+  const { isEmbed } = useIsEmbed()
 
   if (isMobileMode) return <MobileModeBanner forceOpen />
 
   return (
-    <Flex flexFlow="column" p={10} mx="auto" maxW={PAGE_MAX_WIDTH}>
+    <Flex
+      flexFlow="column"
+      p={10}
+      mx="auto"
+      maxW={isEmbed ? PAGE_MAX_WIDTH.embed : PAGE_MAX_WIDTH.standalone}
+    >
       <Header />
       <Outlet />
 
