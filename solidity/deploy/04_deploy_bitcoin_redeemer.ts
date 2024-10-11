4831b57462b9773fcdf8061b1d3617b754d33508
Jakub <jakub.nowakowski@thesis.co>
2024-10-11 11:36:37 +0200

    Check if contract is deployed in the deployment script
    This let us avoid the skip flag in the deployment script.

--- solidity/deploy/04_deploy_bitcoin_redeemer.ts
+++ solidity/deploy/04_deploy_bitcoin_redeemer.ts
@@ -5,34 +5,35 @@ import { waitForTransaction } from "../helpers/deployment"
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { deployments, helpers } = hre
   const { deployer } = await helpers.signers.getNamedSigners()
+  const { log } = deployments
 
   const tbtc = await deployments.get("TBTC")
   const stbtc = await deployments.get("stBTC")
   const tbtcVault = await deployments.get("TBTCVault")
 
-  const [_, deployment] = await helpers.upgrades.deployProxy(
-    "BitcoinRedeemer",
-    {
+  let deployment = await deployments.getOrNull("BitcoinRedeemer")
+  if (deployment && helpers.address.isValid(deployment.address)) {
+    log(`using BitcoinRedeemer at ${deployment.address}`)
+  } else {
+    ;[, deployment] = await helpers.upgrades.deployProxy("BitcoinRedeemer", {
       contractName: "BitcoinRedeemer",
       initializerArgs: [tbtc.address, stbtc.address, tbtcVault.address],
       factoryOpts: { signer: deployer },
       proxyOpts: {
         kind: "transparent",
       },
-    },
-  )
+    })
 
-  if (deployment.transactionHash && hre.network.tags.etherscan) {
-    await waitForTransaction(hre, deployment.transactionHash)
-    await helpers.etherscan.verify(deployment)
+    if (deployment.transactionHash && hre.network.tags.etherscan) {
+      await waitForTransaction(hre, deployment.transactionHash)
+      await helpers.etherscan.verify(deployment)
+    }
+
+    // TODO: Add Tenderly verification
   }
-
-  // TODO: Add Tenderly verification
 }
 
 export default func
 
 func.tags = ["BitcoinRedeemer"]
 func.dependencies = ["TBTC", "stBTC", "TBTCVault"]
-func.skip = async (hre: HardhatRuntimeEnvironment): Promise<boolean> =>
-  Promise.resolve(hre.network.name === "integration")
--- solidity/deploy/41_deploy_multi_asset_vault.ts
+++ solidity/deploy/41_deploy_multi_asset_vault.ts
@@ -5,6 +5,7 @@ import { waitForTransaction } from "../helpers/deployment"
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { deployments, helpers } = hre
   const { deployer, governance } = await helpers.signers.getNamedSigners()
+  const { log } = deployments
 
   const mezoPortal = await deployments.get("MezoPortal")
 
@@ -13,34 +14,36 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     "0xd9D920AA40f578ab794426F5C90F6C731D159DEf", // SolvBTC.BBN
   ]
 
-  const [_, deployment] = await helpers.upgrades.deployProxy(
-    "AcreMultiAssetVault",
-    {
-      contractName: "AcreMultiAssetVault",
-      initializerArgs: [
-        await governance.getAddress(),
-        mezoPortal.address,
-        initialSupportedAssets,
-      ],
-      factoryOpts: { signer: deployer },
-      proxyOpts: {
-        kind: "transparent",
+  let deployment = await deployments.getOrNull("AcreMultiAssetVault")
+  if (deployment && helpers.address.isValid(deployment.address)) {
+    log(`using AcreMultiAssetVault at ${deployment.address}`)
+  } else {
+    ;[, deployment] = await helpers.upgrades.deployProxy(
+      "AcreMultiAssetVault",
+      {
+        contractName: "AcreMultiAssetVault",
+        initializerArgs: [
+          await governance.getAddress(),
+          mezoPortal.address,
+          initialSupportedAssets,
+        ],
+        factoryOpts: { signer: deployer },
+        proxyOpts: {
+          kind: "transparent",
+        },
       },
-    },
-  )
+    )
 
-  if (deployment.transactionHash && hre.network.tags.etherscan) {
-    await waitForTransaction(hre, deployment.transactionHash)
-    await helpers.etherscan.verify(deployment)
+    if (deployment.transactionHash && hre.network.tags.etherscan) {
+      await waitForTransaction(hre, deployment.transactionHash)
+      await helpers.etherscan.verify(deployment)
+    }
+
+    // TODO: Add Tenderly verification
   }
-
-  // TODO: Add Tenderly verification
 }
 
 export default func
 
 func.tags = ["AcreMultiAssetVault"]
 func.dependencies = ["MezoPortal"]
-// TODO: Uncomment after contracts are deployed to mainnet.
-// func.skip = async (hre: HardhatRuntimeEnvironment): Promise<boolean> =>
-//   Promise.resolve(hre.network.name === "integration")
