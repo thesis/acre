d378455584c715a49c54b71ae741b94eb7b7205b
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-10-16 15:46:48 +0200

    Add password form to `GateModal`

--- dapp/src/components/GateModal.tsx
+++ dapp/src/components/GateModal.tsx
@@ -2,12 +2,15 @@ import React from "react"
 import { ModalBody, ModalFooter, ModalHeader } from "@chakra-ui/react"
 import { TextSm } from "#/components/shared/Typography"
 import withBaseModal from "./ModalRoot/withBaseModal"
+import PasswordForm from "./shared/PasswordForm"
 
 export function GateModalBase() {
   return (
     <>
       <ModalHeader>Enter password</ModalHeader>
-      <ModalBody />
+      <ModalBody>
+        <PasswordForm submitButtonText="Connect" onSubmitForm={() => {}} />
+      </ModalBody>
       <ModalFooter>
         <TextSm>Don’t have a password? Contact us on Discord</TextSm>
       </ModalFooter>
