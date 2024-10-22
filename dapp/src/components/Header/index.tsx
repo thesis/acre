bf9a51692fdefcba8ee3330f009b393b512b00f6
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-10-22 20:06:02 +0200

    Delete `Header`'s navigation

--- dapp/src/components/Header/index.tsx
+++ dapp/src/components/Header/index.tsx
@@ -1,10 +1,9 @@
 import React from "react"
 import { AcreLogo } from "#/assets/icons"
-import { Flex, HStack, Icon, Link } from "@chakra-ui/react"
+import { HStack, Icon, Link } from "@chakra-ui/react"
 import { EXTERNAL_HREF } from "#/constants"
 import { useMobileMode } from "#/hooks"
 import ConnectWallet from "./ConnectWallet"
-import { Navigation } from "./Navigation"
 
 export default function Header() {
   const isMobileMode = useMobileMode()
@@ -13,21 +12,15 @@ export default function Header() {
     <HStack
       as="header"
       w="full"
-      mb="4.75rem" // 76px
-      justify="center"
+      justify="space-between"
+      py={7}
+      px="page_content_padding_x"
     >
       <Link href={EXTERNAL_HREF.WEBSITE} isExternal>
         <Icon as={AcreLogo} w={20} h={12} />
       </Link>
 
-      {!isMobileMode && (
-        <>
-          <Navigation />
-          <Flex ml="auto">
-            <ConnectWallet />
-          </Flex>
-        </>
-      )}
+      {!isMobileMode && <ConnectWallet />}
     </HStack>
   )
 }
