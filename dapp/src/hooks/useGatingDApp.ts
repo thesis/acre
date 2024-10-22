40c38dfd52392cb307574c0aa950c249d632de59
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-10-22 14:40:25 +0200

    Remove unneeded changes after rebase

--- dapp/src/hooks/useGatingDApp.ts
+++ dapp/src/hooks/useGatingDApp.ts
@@ -2,20 +2,18 @@ import { useEffect, useRef } from "react"
 import { MODAL_TYPES } from "#/types"
 import { featureFlags } from "#/constants"
 import { useModal } from "./useModal"
-import useIsEmbed from "./useIsEmbed"
 
 export default function useGatingDApp() {
-  const { isEmbed } = useIsEmbed()
   const { openModal } = useModal()
   const isMounted = useRef(false)
 
   useEffect(() => {
-    if (featureFlags.GATING_DAPP_ENABLED && !isMounted.current && !isEmbed) {
+    if (featureFlags.GATING_DAPP_ENABLED && !isMounted.current) {
       isMounted.current = true
       openModal(MODAL_TYPES.GATE, {
         withCloseButton: false,
         closeOnEsc: false,
       })
     }
-  }, [isEmbed, openModal])
+  }, [openModal])
 }
