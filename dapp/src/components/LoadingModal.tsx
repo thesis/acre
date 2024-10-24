3edf103680eade88d334b2bbe8ae1ae95496b4c0
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-10-24 10:04:44 +0200

    Update styles for `LoadingModal`

--- dapp/src/components/LoadingModal.tsx
+++ dapp/src/components/LoadingModal.tsx
@@ -7,7 +7,13 @@ import Spinner from "./shared/Spinner"
 export function LoadingModalBase() {
   return (
     <ModalBody>
-      <Spinner size="xl" />
+      <Spinner
+        size="2xl"
+        variant="filled"
+        borderTopColor="gold.100"
+        borderBottomColor="gold.100"
+        borderLeftColor="gold.100"
+      />
       <TextMd fontWeight="semibold">Loading...</TextMd>
     </ModalBody>
   )
