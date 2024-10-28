0abf23834edde977d575f23aa1593c1fe407d41b
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-10-28 17:05:32 +0100

    Update token amount field value gathering

--- dapp/src/components/TransactionModal/ActionDurationEstimation.tsx
+++ dapp/src/components/TransactionModal/ActionDurationEstimation.tsx
@@ -1,21 +1,24 @@
 import React from "react"
 import { ActivityType } from "#/types"
 import { getEstimatedDuration } from "#/utils"
+import { useFormField } from "#/hooks"
 import { TextMd } from "../shared/Typography"
-import { useTokenAmountField } from "../shared/TokenAmountForm/TokenAmountFormBase"
+import { TOKEN_AMOUNT_FIELD_NAME } from "../shared/TokenAmountForm/TokenAmountFormBase"
 
 export default function ActionDurationEstimation({
   type,
 }: {
   type: ActivityType
 }) {
-  const { value: amount } = useTokenAmountField()
+  const { value: amount = 0n } = useFormField<bigint | undefined>(
+    TOKEN_AMOUNT_FIELD_NAME,
+  )
 
   return (
     <TextMd mt={4} color="grey.400">
       Estimated duration&nbsp;
       <TextMd as="span" color="grey.500">
-        ~ {getEstimatedDuration(amount ?? 0n, type)}
+        ~ {getEstimatedDuration(amount, type)}
       </TextMd>
     </TextMd>
   )
