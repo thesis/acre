42f06c86bebefa1298a9e2aa070af55d4db436e5
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-11-15 15:01:09 +0100

    Remove unnecessary code

--- dapp/src/utils/time.ts
+++ dapp/src/utils/time.ts
@@ -3,10 +3,7 @@ import {
   ONE_DAY_IN_SECONDS,
   ONE_HOUR_IN_SECONDS,
   ONE_MINUTE_IN_SECONDS,
-  ONE_MONTH_IN_SECONDS,
   ONE_SEC_IN_MILLISECONDS,
-  ONE_WEEK_IN_SECONDS,
-  ONE_YEAR_IN_SECONDS,
 } from "#/constants"
 import { TimeUnits } from "#/types"
 import { DateTime } from "luxon"
@@ -30,40 +27,6 @@ export const unixTimestampToTimeUnits = (targetUnix: number): TimeUnits => {
     minutes: minutes.toString(),
     seconds: seconds.toString(),
   }
-}
-
-// unit, max diff, divisor
-const unitsToDivisor: [Intl.RelativeTimeFormatUnit, number, number][] = [
-  ["second", ONE_MINUTE_IN_SECONDS, 1],
-  ["minute", ONE_HOUR_IN_SECONDS, ONE_MINUTE_IN_SECONDS],
-  ["hour", ONE_DAY_IN_SECONDS, ONE_HOUR_IN_SECONDS],
-  ["day", ONE_WEEK_IN_SECONDS, ONE_DAY_IN_SECONDS],
-  ["week", ONE_MONTH_IN_SECONDS, ONE_WEEK_IN_SECONDS],
-  ["month", ONE_YEAR_IN_SECONDS, ONE_MONTH_IN_SECONDS],
-  ["year", Infinity, ONE_YEAR_IN_SECONDS],
-]
-const rtf = new Intl.RelativeTimeFormat(DATE_FORMAT_LOCALE_TAG)
-
-/**
- * The problem of displaying relative time has already been solved in Threshold Network
- * Let's use this logic to be able to display relative time such as: 2 mins ago, 3 hours ago...
- *
- * Source:
- * https://github.com/threshold-network/token-dashboard/blob/main/src/utils/date.ts#L48-L61
- */
-export const getRelativeTime = (dateOrUnixTimestamp: Date | number): string => {
-  const time =
-    typeof dateOrUnixTimestamp === "number"
-      ? dateOrUnixTimestamp
-      : dateToUnixTimestamp(dateOrUnixTimestamp)
-
-  const diff = Math.round(time - dateToUnixTimestamp())
-
-  const [unit, , divisor] =
-    unitsToDivisor.find(([, maxDiff]) => maxDiff > Math.abs(diff)) ??
-    unitsToDivisor[0]
-
-  return rtf.format(Math.floor(diff / divisor), unit)
 }
 
 export const timestampToRelativeTime = (timestamp: number) =>
