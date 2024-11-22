719ac1756336bb6faadff069469b3c9d0704a396
Karolina Kosiorowska <karolina.kosiorowska@thesis.co>
2024-11-22 14:30:23 +0100

    Change padding in layout for smaller screens

--- dapp/src/components/Layout.tsx
+++ dapp/src/components/Layout.tsx
@@ -10,7 +10,10 @@ import Sidebar from "./Sidebar"
 import MobileModeBanner from "./MobileModeBanner"
 import Footer from "./Footer"
 
-const PADDING = "2.5rem" // 40px
+const PADDING = {
+  base: 4,
+  md: "2.5rem", // 40px
+}
 const PAGE_MAX_WIDTH: Record<DappMode, string> = {
   standalone: "63rem", // 1008px
   "ledger-live": "63rem", // 1008px
@@ -33,10 +36,11 @@ function Layout() {
       <Flex
         flexFlow="column"
         mx="auto"
-        p={PADDING}
+        px={PADDING}
+        pb={10}
         pt={0.5}
         w="full"
-        maxWidth={`calc(${maxWidth} + 2*${PADDING})`}
+        maxWidth={`calc(${maxWidth} + 2*${PADDING.md})`}
         flex={1}
       >
         <Outlet />
